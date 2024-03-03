ac60a382ab948ff0215a308211e36e7f3d6446d2
Kadir <kadirht@hotmail.com>
2024-03-03 21:59:03 +0300

    fix: fix folder structure

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import Home from "./Pages/Home";
 import About from "./Pages/About";
 import Contact from "./Pages/Contact";
 import NotFound from "./Pages/NotFound";
-import AppLayout from "./Pages/AppLayout";
+import AppLayout from "./Components/AppLayout";
 
 export default function App() {
     return (
