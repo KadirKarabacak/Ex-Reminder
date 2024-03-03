8e431c0e32e66a514140e95de025654f701f342a
Kadir <kadirht@hotmail.com>
2024-03-03 21:59:22 +0300

    feat: add global styles and darkmodeprovider

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./App.css";
+import GlobalStyles from "./styles/GlobalStyles.ts";
+import { DarkModeProvider } from "./Contexts/DarkModeContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
-        <App />
+        <DarkModeProvider>
+            <GlobalStyles />
+            <App />
+        </DarkModeProvider>
     </React.StrictMode>
 );
