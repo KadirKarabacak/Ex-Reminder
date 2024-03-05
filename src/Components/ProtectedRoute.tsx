da393015d415634394b12795692f9d5e90e06de1
Kadir <kadirht@hotmail.com>
2024-03-05 21:24:40 +0300

    feat: add protected route

---
+++ src/Components/ProtectedRoute.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const FullPage = styled.div`
+    height: 100vh;
+    background-color: var(--color-grey-50);
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`;
+
+export default function ProtectedRoute() {
+    return <FullPage>ProtectedRoute</FullPage>;
+}
