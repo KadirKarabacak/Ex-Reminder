894598e3045934e9ef224f14301df945d697d79a
Kadir <kadirht@hotmail.com>
2024-03-06 18:18:55 +0300

    feat: signin, register

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,7 +1,14 @@
 import { initializeApp } from "firebase/app";
-// import { getAuth, onAuthStateChanged } from "firebase/auth";
+import {
+    createUserWithEmailAndPassword,
+    getAuth,
+    signInWithEmailAndPassword,
+    signOut,
+} from "firebase/auth";
 import { collection, getDocs, getFirestore } from "firebase/firestore";
+import toast from "react-hot-toast";
 
+// Firebase Default Setup
 const firebaseApp = initializeApp({
     apiKey: "AIzaSyCTDVf4r8rsaIL1gRMnXl6tmGrC-Cl4e1w",
     authDomain: "my-router-project.firebaseapp.com",
@@ -12,12 +19,75 @@ const firebaseApp = initializeApp({
     measurementId: "G-9H14BENC87",
 });
 const db = getFirestore(firebaseApp);
+const auth = getAuth();
 
-const getUsers = async () => {
+// Get All Users
+export const getUsers = async () => {
     const querySnapShot = await getDocs(collection(db, "users"));
     querySnapShot.forEach(doc => {
-        // QJWrqzU0WtrHqlgoOQ7z, firstName - lastName - email
         console.log(doc.id, doc.data());
     });
 };
-getUsers();
+
+interface loginTypes {
+    email: string;
+    password: string;
+}
+
+// Login
+export const signInWithEmailAndPasswordQuery = async ({
+    email,
+    password,
+}: loginTypes) => {
+    try {
+        const userCredential = await signInWithEmailAndPassword(
+            auth,
+            email,
+            password
+        );
+        const user = userCredential.user;
+        if (user) {
+            toast.success("Successfully logged in, redirecting...");
+            return true;
+        }
+    } catch (error) {
+        toast.error(
+            "Wrong email or password. Check your credentials and try again"
+        );
+    }
+    return false;
+};
+
+// Create new user
+export const createUserWithEmailAndPasswordQuery = async ({
+    email,
+    password,
+}: loginTypes) => {
+    try {
+        const userCredential = await createUserWithEmailAndPassword(
+            auth,
+            email,
+            password
+        );
+        const user = userCredential.user;
+        if (user) {
+            toast.success("User successfully created");
+            return true;
+        }
+    } catch (error) {
+        toast.error("There was an error creating the user.");
+    }
+    return false;
+};
+
+// Logout
+export const logOut = async () => {
+    signOut(auth)
+        .then(() => {
+            console.log("Logged out");
+            toast.success("Successfully logged out");
+        })
+        .catch(error => {
+            toast.error(`Something went wrong with ${error.message}`);
+        });
+};
