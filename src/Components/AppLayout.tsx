eaf4b4ae6bd21b2c9f1ec288fac51ff4bc8be020
Kadir <kadirht@hotmail.com>
2024-03-06 20:40:36 +0300

    feat: add protected route

--- src/Components/AppLayout.tsx
+++ src/Components/AppLayout.tsx
@@ -3,6 +3,7 @@ import Sidebar from "./Sidebar";
 import Header from "./Header";
 import styled from "styled-components";
 import { Toaster } from "react-hot-toast";
+import ProtectedRoute from "./ProtectedRoute";
 
 const StyledAppLayout = styled.div`
     display: grid;
@@ -28,35 +29,37 @@ const Container = styled.div`
 // Parent Route
 function AppLayout() {
     return (
-        <StyledAppLayout>
-            <Header />
-            <Sidebar />
-            <Main>
-                <Container>
-                    <Outlet />
-                </Container>
-            </Main>
-            <Toaster
-                position="bottom-left"
-                gutter={12}
-                containerStyle={{ margin: "8px" }}
-                toastOptions={{
-                    success: {
-                        duration: 4000,
-                    },
-                    error: {
-                        duration: 5000,
-                    },
-                    style: {
-                        fontSize: "16px",
-                        maxWidth: "500px",
-                        padding: "16px 24px",
-                        backgroundColor: "var(--color-grey-0)",
-                        color: "var(--color-grey-700)",
-                    },
-                }}
-            />
-        </StyledAppLayout>
+        <ProtectedRoute>
+            <StyledAppLayout>
+                <Header />
+                <Sidebar />
+                <Main>
+                    <Container>
+                        <Outlet />
+                    </Container>
+                </Main>
+                <Toaster
+                    position="bottom-left"
+                    gutter={12}
+                    containerStyle={{ margin: "8px" }}
+                    toastOptions={{
+                        success: {
+                            duration: 4000,
+                        },
+                        error: {
+                            duration: 5000,
+                        },
+                        style: {
+                            fontSize: "16px",
+                            maxWidth: "500px",
+                            padding: "16px 24px",
+                            backgroundColor: "var(--color-grey-0)",
+                            color: "var(--color-grey-700)",
+                        },
+                    }}
+                />
+            </StyledAppLayout>
+        </ProtectedRoute>
     );
 }
 
