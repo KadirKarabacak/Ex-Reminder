3d9c86e4dc2cfb1823d09bdfc875126f5de7d8d4
Kadir <kadirht@hotmail.com>
2024-03-06 20:40:49 +0300

    feat: add protected route

--- src/Components/ProtectedRoute.tsx
+++ src/Components/ProtectedRoute.tsx
@@ -1,13 +1,42 @@
-import styled from "styled-components";
+// import styled from "styled-components";
+import { onAuthStateChanged } from "firebase/auth";
+import { ReactNode, useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { auth } from "../Api/firebase";
 
-const FullPage = styled.div`
-    height: 100vh;
-    background-color: var(--color-grey-50);
-    display: flex;
-    align-items: center;
-    justify-content: center;
-`;
+//: Daha sonra spinner için kullanılabilir.
+// const FullPage = styled.div`
+//     height: 100vh;
+//     background-color: var(--color-grey-50);
+//     display: flex;
+//     align-items: center;
+//     justify-content: center;
+// `;
 
-export default function ProtectedRoute() {
-    return <FullPage>ProtectedRoute</FullPage>;
+interface ProtectedRouteProps {
+    children: ReactNode;
 }
+
+const ProtectedRoute: React.FC<ProtectedRouteProps> = ({ children }) => {
+    const navigate = useNavigate();
+    const [isAuthenticated, setIsAuthenticated] = useState(false);
+    const { currentUser } = auth;
+    console.log(currentUser);
+
+    // Handle authenticated user
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, user => {
+            if (user) {
+                setIsAuthenticated(true);
+            } else {
+                navigate("/login");
+            }
+        });
+
+        return () => unsubscribe();
+    }, [navigate]);
+
+    if (isAuthenticated) return children;
+};
+
+export default ProtectedRoute;
