e881d7028cc9bd00a0943cb5ef3abb11f82329a4
Kadir <kadirht@hotmail.com>
2024-03-06 18:19:41 +0300

    feat: add register page

---
+++ src/Pages/Register.tsx
@@ -0,0 +1,36 @@
+import styled, { keyframes } from "styled-components";
+import RegisterForm from "../Components/RegisterForm";
+
+// Keyframes tanımlama
+const circleInHesitate = keyframes`
+  0% {
+    clip-path: circle(0%);
+  }
+  40% {
+    clip-path: circle(40%);
+  }
+  100% {
+    clip-path: circle(125%);
+  }
+`;
+
+const StyledRegister = styled.main`
+    width: 100%;
+    height: 100dvh;
+    background-color: var(--color-grey-200);
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: 2.5rem;
+
+    animation: 2.2s cubic-bezier(0.25, 1, 0.3, 1) ${circleInHesitate} both;
+`;
+
+export default function Login() {
+    return (
+        <StyledRegister>
+            <RegisterForm />
+        </StyledRegister>
+    );
+}
