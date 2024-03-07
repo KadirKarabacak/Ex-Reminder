2ba06752317a2f86df69a117eb912497125addb1
Kadir <kadirht@hotmail.com>
2024-03-07 19:49:43 +0300

    feat: add user avatar and title

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -8,6 +8,7 @@ import Tooltip from "@mui/material/Tooltip";
 import Zoom from "@mui/material/Zoom";
 import { Link } from "react-router-dom";
 import { logOut } from "../Api/userController";
+import { auth } from "../Api/firebase";
 
 const StyledHeader = styled.header`
     background-color: var(--color-grey-0);
@@ -29,6 +30,24 @@ const StyledList = styled.ul`
 const StyledListItem = styled.li`
     display: flex;
     align-items: center;
+    gap: 1rem;
+`;
+
+const StyledUserInfo = styled.div`
+    display: flex;
+    gap: 0.5rem;
+    align-items: center;
+    justify-content: center;
+`;
+
+const StyledAvatar = styled.img`
+    width: 4rem;
+    display: block;
+    aspect-ratio: 1/1;
+    object-fit: cover;
+    object-position: center center;
+    border-radius: 50%;
+    outline: 2px solid var(--color-grey-800);
 `;
 
 const iconStyle = {
@@ -39,11 +58,22 @@ const iconStyle = {
 
 function Header() {
     const { isDarkMode, toggleDarkMode } = useDarkMode();
+    const { currentUser } = auth;
 
     return (
         <StyledHeader>
             <StyledList>
-                <StyledListItem>User</StyledListItem>
+                <StyledUserInfo>
+                    <StyledListItem>
+                        {currentUser?.photoURL && (
+                            <StyledAvatar
+                                src={currentUser.photoURL}
+                                alt="User Avatar"
+                            />
+                        )}
+                        {currentUser?.displayName}
+                    </StyledListItem>
+                </StyledUserInfo>
                 <StyledListItem>
                     <Button
                         sx={{ fontSize: "2rem", minWidth: 0, p: "0.7rem" }}
