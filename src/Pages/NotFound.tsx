054bbb7fa630bcb513c0e2d3332df30db6a7c7a1
Kadir <kadirht@hotmail.com>
2024-03-08 20:02:22 +0300

    styling: add styling to notfound page

--- src/Pages/NotFound.tsx
+++ src/Pages/NotFound.tsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 import styled from "styled-components";
 
 const StyledNotFound = styled.main`
@@ -8,22 +8,35 @@ const StyledNotFound = styled.main`
     background-color: var(--color-grey-100);
     text-align: center;
     display: flex;
+    flex-direction: column;
     justify-content: center;
-    padding-top: 1rem;
+    align-items: center;
+    gap: 2rem;
 `;
 
 const StyledLink = styled(Link)`
     text-decoration: none;
-    color: var(--color-grey-800);
+    color: var(--color-indigo-700);
     font-size: 2rem;
     margin-right: 1rem;
+    background-color: var(--color-grey-100);
+`;
+
+const StyledText = styled.p`
+    font-size: 2.5rem;
+    font-weight: bold;
 `;
 
 export default function NotFound() {
     return (
         <StyledNotFound>
-            NotFound
-            <StyledLink to="/">Go back</StyledLink>
+            <StyledText>
+                Sorry, the page you are looking for does not exist. 😥
+            </StyledText>
+            <StyledLink to="/">
+                <ArrowBackIcon sx={{ fontSize: "2rem" }} />
+                Go back
+            </StyledLink>
         </StyledNotFound>
     );
 }
