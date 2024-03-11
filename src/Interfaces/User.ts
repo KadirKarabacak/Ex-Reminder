05acc345caa76753e7925e5c423b25d73f308ba3
Kadir <kadirht@hotmail.com>
2024-03-11 17:10:08 +0300

    feat: add types

--- src/Interfaces/User.ts
+++ src/Interfaces/User.ts
@@ -9,6 +9,23 @@ export interface CurrentUserTypes {
     photoURL: any;
 }
 
+export interface UpdateUserPasswordTypes {
+    currentUser: any;
+    password: string;
+    newPassword: string;
+}
+
+export interface UpdateUserEmailTypes {
+    currentUser: any;
+    email: string;
+    password: string;
+}
+
+export interface DeleteUserTypes {
+    currentUser: any;
+    password: string;
+}
+
 export interface ModalTypes {
     open: boolean;
     handleClose: React.Dispatch<React.SetStateAction<boolean>>;
