04cc0d2f320e2d6d8e9ed8972d780d0be49a7571
Kadir <kadirht@hotmail.com>
2024-03-15 22:33:07 +0300

    feat: add localization for datepicker

--- src/main.tsx
+++ src/main.tsx
@@ -6,12 +6,16 @@ import GlobalStyles from "./styles/GlobalStyles.ts";
 import { DarkModeProvider } from "./Contexts/DarkModeContext.jsx";
 import "./Api/userController.ts";
 import "./i18n";
+import { LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFnsV3";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
-        <DarkModeProvider>
-            <GlobalStyles />
-            <App />
-        </DarkModeProvider>
+        <LocalizationProvider dateAdapter={AdapterDateFns}>
+            <DarkModeProvider>
+                <GlobalStyles />
+                <App />
+            </DarkModeProvider>
+        </LocalizationProvider>
     </React.StrictMode>
 );
