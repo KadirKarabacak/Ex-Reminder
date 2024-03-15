6f2595f358913e0383f521f248c8aea54e5b0c4c
Kadir <kadirht@hotmail.com>
2024-03-15 22:34:40 +0300

    feat: add new util

--- src/Utils/utils.ts
+++ src/Utils/utils.ts
@@ -1,3 +1,10 @@
 export const extractFileName = (file: any) => {
     return file ? file?.name?.slice(0, 25) + "..." : "";
 };
+
+export function formatDate(date: any) {
+    const day = String(date.getDate()).padStart(2, "0");
+    const month = String(date.getMonth() + 1).padStart(2, "0");
+    const year = date.getFullYear();
+    return `${day}/${month}/${year}`;
+}
