49d77a761eaf790cf9403470f6f7abf6354f4c93
Kadir <kadirht@hotmail.com>
2024-03-15 22:34:13 +0300

    feat: employee bar

--- src/Components/TableToolBars/EmployeeBar.tsx
+++ src/Components/TableToolBars/EmployeeBar.tsx
@@ -8,6 +8,8 @@ import {
 import { useTranslation } from "react-i18next";
 import styled from "styled-components";
 import FilterListIcon from "@mui/icons-material/FilterList";
+import { useState } from "react";
+import AddEmployeeModal from "../Modals/AddEmployeeModal";
 
 const StyledIconButton = styled(IconButton)`
     font-size: 1.8rem;
@@ -18,58 +20,67 @@ const StyledIconButton = styled(IconButton)`
 
 export function EmployeeToolBar() {
     const { t } = useTranslation();
-    return (
-        <Toolbar
-            sx={{
-                pl: { sm: 2 },
-                pr: { xs: 1, sm: 1 },
-                gap: "1rem",
-            }}
-        >
-            <Typography
-                sx={{
-                    marginRight: "auto",
-                    color: "var(--color-grey-800)",
-                    fontSize: "1.3rem",
-                    fontWeight: "bold",
-                }}
-                variant="subtitle1"
-                component="div"
-            >
-                {t("Employees")}
-            </Typography>
+    const [open, setOpen] = useState(false);
 
-            {/* EMPLOYEE BUTTON OPENS MODAL */}
-            <Button
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
+    return (
+        <>
+            <Toolbar
                 sx={{
-                    backgroundColor: "var(--color-grey-800)",
-                    color: "var(--color-grey-50)",
-                    transition: "all .3s",
-                    padding: "1rem 2rem",
-                    fontSize: "1rem",
-                    alignSelf: "center",
-                    fontWeight: "bold",
-                    "&:hover": {
-                        backgroundColor: "var(--color-grey-600)",
-                        color: "var(--color-grey-100)",
-                        transform: "translateY(-2px)",
-                    },
-                    "&:active": {
-                        transform: "translateY(0)",
-                    },
-                    "&:disabled": {
-                        backgroundColor: "var(--color-grey-500)",
-                    },
+                    pl: { sm: 2 },
+                    pr: { xs: 1, sm: 1 },
+                    gap: "1rem",
                 }}
-                variant="contained"
             >
-                {t("Add Employee")}
-            </Button>
-            <Tooltip title="Filter list">
-                <StyledIconButton>
-                    <FilterListIcon />
-                </StyledIconButton>
-            </Tooltip>
-        </Toolbar>
+                <Typography
+                    sx={{
+                        marginRight: "auto",
+                        color: "var(--color-grey-800)",
+                        fontSize: "1.3rem",
+                        fontWeight: "bold",
+                    }}
+                    variant="subtitle1"
+                    component="div"
+                >
+                    {t("Employees")}
+                </Typography>
+
+                {/* EMPLOYEE BUTTON OPENS MODAL */}
+                <Button
+                    onClick={handleOpen}
+                    sx={{
+                        backgroundColor: "var(--color-grey-800)",
+                        color: "var(--color-grey-50)",
+                        transition: "all .3s",
+                        padding: "1rem 2rem",
+                        fontSize: "1rem",
+                        alignSelf: "center",
+                        fontWeight: "bold",
+                        "&:hover": {
+                            backgroundColor: "var(--color-grey-600)",
+                            color: "var(--color-grey-100)",
+                            transform: "translateY(-2px)",
+                        },
+                        "&:active": {
+                            transform: "translateY(0)",
+                        },
+                        "&:disabled": {
+                            backgroundColor: "var(--color-grey-500)",
+                        },
+                    }}
+                    variant="contained"
+                >
+                    {t("Add Employee")}
+                </Button>
+                <Tooltip title="Filter list">
+                    <StyledIconButton>
+                        <FilterListIcon />
+                    </StyledIconButton>
+                </Tooltip>
+            </Toolbar>
+            {/* AddEmployeeModal */}
+            <AddEmployeeModal handleClose={handleClose} open={open} />
+        </>
     );
 }
