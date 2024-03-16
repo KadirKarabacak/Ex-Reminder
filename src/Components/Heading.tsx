741da9bc34c34d4e08021aae0756706df765a084
Kadir <kadirht@hotmail.com>
2024-03-17 02:47:00 +0300

    fix: remove types from files

--- src/Components/Heading.tsx
+++ src/Components/Heading.tsx
@@ -1,14 +1,11 @@
 import styled from "styled-components";
+import { HeadingProps } from "../Interfaces/User";
 
 const StyledHeading = styled.h2`
     font-size: 3.5rem;
     color: var(--color-grey-800);
     font-weight: bold;
 `;
-
-interface HeadingProps {
-    title: string;
-}
 
 export default function Heading({ title }: HeadingProps) {
     return <StyledHeading>{title}</StyledHeading>;
