23fbb190711dd91dafabc1a4e52683a0a6bd35f1
Kadir <kadirht@hotmail.com>
2024-03-22 16:21:58 +0300

    fix: fix styling

--- src/Components/Forms/LoginForm.tsx
+++ src/Components/Forms/LoginForm.tsx
@@ -23,20 +23,32 @@ const StyledLogo = styled.img`
 `;
 
 const StyledTextField = styled(TextField)`
+    width: 100%;
+    background-color: var(--color-grey-200);
+    border-top-left-radius: 3px;
+    border-top-right-radius: 3px;
+
     & div + p {
         font-size: 1rem;
     }
     & label {
-        color: var(--color-grey-800);
+        color: var(--color-grey-400);
+        font-size: 1.3rem;
     }
+
     & div > input {
-        color: var(--color-grey-800);
+        color: var(--color-grey-700);
         font-size: 1.3rem;
 
         &:disabled {
             background-color: var(--color-grey-300);
         }
     }
+
+    & div > input:focus .MuiInputLabel-filled {
+        color: var(--color-brand-500);
+    }
+
     & div > fieldset {
         border-color: var(--color-grey-500);
     }
@@ -161,7 +173,7 @@ export default function LoginForm() {
                     disabled={isSubmitting}
                     label="Email"
                     sx={{ minWidth: "100%" }}
-                    variant="outlined"
+                    variant="filled"
                     {...register("email", {
                         required: t("Email is required"),
                         pattern: {
@@ -179,7 +191,7 @@ export default function LoginForm() {
                     disabled={isSubmitting}
                     label={t("Password")}
                     sx={{ minWidth: "100%" }}
-                    variant="outlined"
+                    variant="filled"
                     {...register("password", {
                         required: t("Password is required"),
                     })}
@@ -199,6 +211,7 @@ export default function LoginForm() {
                             transition: "all .3s",
                             padding: "1rem 3rem",
                             fontSize: "1.1rem",
+                            fontWeight: "bold",
                             "&:hover": {
                                 backgroundColor: "var(--color-grey-600)",
                                 color: "var(--color-grey-100)",
@@ -224,8 +237,7 @@ export default function LoginForm() {
                             fontSize: "1.1rem",
                             border: "1px solid var(--color-grey-800)",
                             "&:hover": {
-                                backgroundColor: "var(--color-grey-800)",
-                                color: "var(--color-grey-100)",
+                                backgroundColor: "var(--color-grey-200)",
                                 transform: "translateY(-2px)",
                                 border: "1px solid var(--color-grey-800)",
                             },
