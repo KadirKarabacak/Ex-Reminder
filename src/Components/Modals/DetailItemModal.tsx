5afab2050f02c4b48d0f6463c1539e921fb30271
Kadir <kadirht@hotmail.com>
2024-03-24 19:07:06 +0300

    fix

--- src/Components/Modals/DetailItemModal.tsx
+++ src/Components/Modals/DetailItemModal.tsx
@@ -164,7 +164,7 @@ export default function DetailItemModal({
                                     ? t("Profit")
                                     : profit < 0
                                     ? t("Loss")
-                                    : t("Fit")}
+                                    : t("Profit | Loss")}
                             </StyledTitle>
                             <StyledDescription>
                                 {(
