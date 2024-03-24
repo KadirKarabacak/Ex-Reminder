dbfc9a58728c06ec388948212eeda3e2495e505b
Kadir <kadirht@hotmail.com>
2024-03-24 19:07:24 +0300

    feat: add company table row

---
+++ src/Components/TableRows/CompanyTableRow.tsx
@@ -0,0 +1,77 @@
+import { Checkbox, TableCell, TableRow } from "@mui/material";
+import ButtonGroup from "../ButtonGroup";
+
+const TableCellStyles = {
+    color: "var(--color-grey-600)",
+    fontSize: "1.2rem",
+    textAlign: "left",
+    borderBottom: "1px solid var(--color-grey-200)",
+};
+
+interface EmployeeTableRowTypes {
+    isItemSelected: any;
+    handleClick: any;
+    index: number;
+    labelId: string;
+    row: any;
+}
+
+export default function CompanyTableRow({
+    isItemSelected,
+    handleClick,
+    index,
+    labelId,
+    row,
+}: EmployeeTableRowTypes) {
+    return (
+        <TableRow
+            hover
+            aria-checked={isItemSelected}
+            tabIndex={-1}
+            selected={isItemSelected}
+        >
+            <TableCell
+                onClick={event => handleClick(event, index)}
+                padding="checkbox"
+                sx={{
+                    ...TableCellStyles,
+                    cursor: "pointer",
+                }}
+            >
+                <Checkbox
+                    checked={isItemSelected}
+                    inputProps={{
+                        "aria-labelledby": labelId,
+                    }}
+                    sx={{
+                        color: "var(--color-grey-800)",
+                    }}
+                />
+            </TableCell>
+            <TableCell
+                component="th"
+                id={labelId}
+                scope="row"
+                padding="none"
+                sx={TableCellStyles}
+            >
+                {row.companyName || "-"}
+            </TableCell>
+            <TableCell align="right" sx={TableCellStyles}>
+                {row.companyAddress || "-"}
+            </TableCell>
+            <TableCell align="right" sx={TableCellStyles}>
+                {row.companyPhone || "-"}
+            </TableCell>
+            <TableCell align="right" sx={TableCellStyles}>
+                {row.companyEmail || "-"}
+            </TableCell>
+            <TableCell align="right" sx={TableCellStyles}>
+                {row.companyDescription || "-"}
+            </TableCell>
+            <TableCell align="right" sx={TableCellStyles}>
+                <ButtonGroup tableName="company" row={row} />
+            </TableCell>
+        </TableRow>
+    );
+}
