8524cfcec0211a1d4af650b37f17623b5f7efb25
Kadir <kadirht@hotmail.com>
2024-03-25 21:35:07 +0300

    feat: add agreement modal

---
+++ src/Components/Modals/AddAgreementModal.tsx
@@ -0,0 +1,374 @@
+import {
+    Backdrop,
+    Box,
+    Button,
+    Divider,
+    Fade,
+    Grid,
+    Modal,
+    TextField,
+    Typography,
+} from "@mui/material";
+import styled from "styled-components";
+import { useForm } from "react-hook-form";
+import { useTranslation } from "react-i18next";
+import React, { useState } from "react";
+// import { MuiTelInput } from "mui-tel-input";
+import { min, sub } from "date-fns";
+import { DatePicker, DateValidationError } from "@mui/x-date-pickers";
+import { useAddAgreement } from "../../Api/companyController";
+import { formatCurrency, formatDate } from "../../Utils/utils";
+
+const StyledBox = styled(Box)`
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    min-width: 35%;
+    background-color: var(--color-grey-100);
+    border: none;
+    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.3);
+    padding: 4rem 4rem 3rem;
+    border-radius: 5px;
+`;
+const StyledButtonContainer = styled.div`
+    display: flex;
+    gap: 1.5rem;
+    margin-top: 2rem;
+    justify-content: center;
+`;
+
+const StyledTextField = styled(TextField)`
+    width: 100%;
+    & div + p {
+        font-size: 1rem;
+    }
+    & label {
+        color: var(--color-grey-400);
+        font-size: 1.2rem;
+    }
+    & div > input {
+        color: var(--color-grey-800);
+        font-size: 1.3rem;
+
+        &:disabled {
+            background-color: var(--color-grey-300);
+        }
+    }
+    & div > fieldset {
+        border-color: var(--color-grey-500);
+    }
+    &:hover > div > fieldset {
+        border-color: var(--color-brand-600) !important;
+    }
+`;
+
+const StyledTitle = styled.h4`
+    color: var(--color-grey-800);
+    align-self: flex-start;
+    margin-bottom: 0.9rem;
+`;
+
+// const StyledTelInput = styled(MuiTelInput)`
+//     width: 100%;
+//     & > div {
+//         color: var(--color-grey-800);
+//         font-size: 1.3rem;
+//         padding-left: 10px;
+//     }
+
+//     & > div > fieldset {
+//         border-color: var(--color-grey-500);
+//     }
+
+//     &:hover > div > fieldset {
+//         border-color: var(--color-brand-600) !important;
+//     }
+// `;
+
+const StyledSpan = styled.span`
+    color: var(--color-brand-500);
+    font-size: 3rem;
+    border-left: 2px solid var(--color-grey-500);
+    padding-left: 8px;
+`;
+
+const StyledDatePicker = styled(DatePicker)`
+    width: 100%;
+
+    & > div {
+        color: var(--color-grey-800);
+        font-size: 1.3rem;
+
+        &:hover > fieldset {
+            border-color: var(--color-brand-600) !important;
+        }
+
+        & > fieldset {
+            border-color: var(--color-grey-500);
+        }
+    }
+`;
+
+const minDate = min([new Date(2000, 1, 1)]);
+
+interface AgreementModalTypes {
+    open: boolean;
+    handleClose: React.Dispatch<React.SetStateAction<boolean>>;
+    currentCompany: any;
+}
+
+export default function AddAgreementModal({
+    open,
+    handleClose,
+    currentCompany,
+}: AgreementModalTypes) {
+    const { t } = useTranslation();
+    const [agreementStart, setAgreementStart] = useState(
+        sub(new Date(), { months: 1 })
+    );
+    const [agreementEnd, setAgreementEnd] = useState(new Date());
+    const [error, setError] = useState<DateValidationError>(null);
+    const { mutateAsync: addAgreement, isPending } = useAddAgreement();
+    const companyId = currentCompany?.id;
+
+    const errorMessage = React.useMemo(() => {
+        switch (error) {
+            case "minDate": {
+                return t("Date cannot be before 10/06/2000");
+            }
+            case "invalidDate": {
+                return t("Your date is not valid");
+            }
+
+            default: {
+                return "";
+            }
+        }
+    }, [error]);
+
+    const {
+        handleSubmit,
+        register,
+        getValues,
+        clearErrors,
+        reset,
+        setValue,
+        formState: { errors },
+    } = useForm();
+
+    async function onSubmit() {
+        const { agreementDescription, agreementParties, agreementBudget } =
+            getValues();
+
+        let budget;
+        if (agreementBudget) budget = formatCurrency(agreementBudget);
+
+        const agreement = {
+            agreementDescription,
+            agreementParties,
+            agreementBudget: budget || "",
+            agreementStartDate: formatDate(agreementStart),
+            agreementEndDate: formatDate(agreementEnd),
+            createdAt: formatDate(new Date()),
+        };
+        await addAgreement({ agreement, companyId });
+        //! BİR ANLAŞMA BELGESİ VAR İSE KULLANICININ ONU SİSTEME YÜKLEMESİNE İZİN VER
+        //! ANLAŞMA OLUŞTURULDUKTAN SONRA "SUB" FONKSİYONU İLE BİTİŞ TARİHİNDEN BAŞLANGIÇ TARİHİNİ ÇIKARARAK KALAN ANLAŞMA SÜRESİNİ HESAPLA
+        onCloseModal();
+    }
+
+    function onCloseModal() {
+        handleClose(open);
+        clearErrors();
+        reset();
+    }
+
+    return (
+        <Modal
+            aria-labelledby="transition-modal-title"
+            aria-describedby="transition-modal-description"
+            open={open}
+            onClose={onCloseModal}
+            closeAfterTransition
+            slots={{ backdrop: Backdrop }}
+            slotProps={{
+                backdrop: {
+                    timeout: 500,
+                },
+            }}
+        >
+            <Fade in={open}>
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    <StyledBox>
+                        <Typography
+                            id="transition-modal-title"
+                            variant="h3"
+                            component="h1"
+                            sx={{ fontWeight: "bold", letterSpacing: "0.80px" }}
+                        >
+                            {t("Add Agreement with")}{" "}
+                            <StyledSpan>
+                                {currentCompany.companyName}
+                            </StyledSpan>
+                        </Typography>
+                        <Grid container spacing={2} sx={{ mt: "1rem" }}>
+                            <Grid item xs={6}>
+                                <StyledTitle>
+                                    {t("Agreement Description")}
+                                </StyledTitle>
+                                <StyledTextField
+                                    label={t("Agreement Description")}
+                                    {...register("agreementDescription", {
+                                        required: t(
+                                            "Agreement Description is required"
+                                        ),
+                                    })}
+                                    error={Boolean(
+                                        errors?.agreementDescription
+                                    )}
+                                    helperText={
+                                        (errors?.agreementDescription
+                                            ?.message as React.ReactNode) || ""
+                                    }
+                                />
+                            </Grid>
+                            <Grid item xs={6}>
+                                <StyledTitle>
+                                    {t("Agreement Budget")}
+                                </StyledTitle>
+                                <StyledTextField
+                                    type="number"
+                                    label={t("Agreement Budget")}
+                                    {...register("agreementBudget")}
+                                    error={Boolean(errors?.agreementBudget)}
+                                    helperText={
+                                        (errors?.agreementBudget
+                                            ?.message as React.ReactNode) || ""
+                                    }
+                                />
+                            </Grid>
+                            <Grid item xs={12}>
+                                <Divider
+                                    sx={{
+                                        borderColor: "var(--color-grey-200)",
+                                    }}
+                                />
+                            </Grid>
+                            <Grid item xs={4}>
+                                <StyledTitle>
+                                    {t("Parties to the Agreement")}
+                                </StyledTitle>
+                                <StyledTextField
+                                    type="text"
+                                    label={t("Parties to the Agreement")}
+                                    {...register("agreementParties")}
+                                />
+                            </Grid>
+
+                            <Grid item xs={4}>
+                                <StyledTitle>
+                                    {t("Agreement Start Date")}
+                                </StyledTitle>
+                                <StyledDatePicker
+                                    format="dd/MM/yyyy"
+                                    onChange={(date: any) => {
+                                        setValue("agreementStartDate", date);
+                                        setAgreementStart(date);
+                                    }}
+                                    value={agreementStart}
+                                    defaultValue={new Date()}
+                                    onError={newError => setError(newError)}
+                                    slotProps={{
+                                        textField: {
+                                            helperText: errorMessage,
+                                        },
+                                    }}
+                                    minDate={minDate}
+                                />
+                            </Grid>
+                            <Grid item xs={4}>
+                                <StyledTitle>
+                                    {t("Agreement End Date")}
+                                </StyledTitle>
+                                <StyledDatePicker
+                                    format="dd/MM/yyyy"
+                                    onChange={(date: any) => {
+                                        setValue("agreementEndDate", date);
+                                        setAgreementEnd(date);
+                                    }}
+                                    value={agreementEnd}
+                                    defaultValue={new Date()}
+                                    onError={newError => setError(newError)}
+                                    slotProps={{
+                                        textField: {
+                                            helperText: errorMessage,
+                                        },
+                                    }}
+                                    minDate={minDate}
+                                />
+                            </Grid>
+                            <Grid item xs={12}>
+                                <Divider
+                                    sx={{
+                                        borderColor: "var(--color-grey-200)",
+                                    }}
+                                />
+                            </Grid>
+                        </Grid>
+
+                        <StyledButtonContainer>
+                            <Button
+                                sx={{
+                                    backgroundColor: "var(--color-grey-800)",
+                                    color: "var(--color-grey-50)",
+                                    transition: "all .3s",
+                                    padding: "1rem 2rem",
+                                    fontSize: "1.1rem",
+                                    alignSelf: "flex-start",
+                                    fontWeight: "bold",
+                                    "&:hover": {
+                                        backgroundColor:
+                                            "var(--color-grey-700)",
+                                        transform: "translateY(-2px)",
+                                    },
+                                    "&:active": {
+                                        transform: "translateY(0)",
+                                    },
+                                }}
+                                type="submit"
+                                variant="contained"
+                            >
+                                {t("Complate Agreement")}
+                            </Button>
+                            <Button
+                                onClick={onCloseModal}
+                                sx={{
+                                    color: "var(--color-grey-800)",
+                                    transition: "all .3s",
+                                    padding: "1rem 3rem",
+                                    fontSize: "1.1rem",
+                                    border: "1px solid var(--color-grey-500)",
+                                    backgroundColor: "var(--color-grey-100)",
+                                    fontWeight: "bold",
+                                    "&:hover": {
+                                        backgroundColor:
+                                            "var(--color-grey-200)",
+                                        transform: "translateY(-2px)",
+                                        border: "1px solid var(--color-grey-800)",
+                                    },
+                                    "&:active": {
+                                        transform: "translateY(0)",
+                                    },
+                                }}
+                                variant="outlined"
+                            >
+                                {t("Cancel")}
+                            </Button>
+                        </StyledButtonContainer>
+                    </StyledBox>
+                </form>
+            </Fade>
+        </Modal>
+    );
+}
