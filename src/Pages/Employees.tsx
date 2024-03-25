364e7f08ff442b88a3aca9493424a38f3081478e
Kadir <kadirht@hotmail.com>
2024-03-25 21:37:02 +0300

    chore: remove unused code

--- src/Pages/Employees.tsx
+++ src/Pages/Employees.tsx
@@ -7,10 +7,7 @@ import { InfinitySpin } from "react-loader-spinner";
 import { useGetEmployees } from "../Api/employeeController";
 import { CustomPieChart } from "../Components/CustomPieChart";
 import EmployeeStats from "../Components/EmployeeStats";
-import EditIcon from "@mui/icons-material/Edit";
-import { DeleteOutline } from "@mui/icons-material";
-import ReadMoreIcon from "@mui/icons-material/ReadMore";
-import React from "react";
+
 import { Helmet } from "react-helmet";
 import { useTranslation } from "react-i18next";
 
@@ -35,8 +32,9 @@ const FullPage = styled.div`
 `;
 
 const InformationContainer = styled.div`
-    display: flex;
+    display: grid;
     gap: 1.5rem;
+    grid-template-columns: 1fr 1fr;
 `;
 
 const AnimatedStyledEmployees = animated(StyledEmployees);
@@ -44,35 +42,8 @@ const AnimatedStyledEmployees = animated(StyledEmployees);
 export default function Employees() {
     const animationProps = useSpring(springOptions);
     const { data, isLoading } = useGetEmployees();
-    const [opensEdit, setOpensEdit] = React.useState(false);
-    const [opensDelete, setOpensDelete] = React.useState(false);
-    const [opensDetail, setOpensDetail] = React.useState(false);
-    const employee = true;
-    const { t } = useTranslation();
 
-    const buttonGroups = [
-        {
-            label: "Edit",
-            handleOpen: () => setOpensEdit(true),
-            handleClose: () => setOpensEdit(false),
-            opens: opensEdit,
-            startIcon: <EditIcon />,
-        },
-        {
-            label: "Detail",
-            handleOpen: () => setOpensDetail(true),
-            handleClose: () => setOpensDetail(false),
-            opens: opensDelete,
-            startIcon: <ReadMoreIcon />,
-        },
-        {
-            label: "Delete",
-            handleOpen: () => setOpensDelete(true),
-            handleClose: () => setOpensDelete(false),
-            opens: opensDetail,
-            startIcon: <DeleteOutline />,
-        },
-    ];
+    const { t } = useTranslation();
 
     if (isLoading)
         return (
@@ -86,11 +57,7 @@ export default function Employees() {
             <Helmet>
                 <title>Ex Reminder | {t("Employees")}</title>
             </Helmet>
-            <CustomTable
-                CustomToolbar={<EmployeeToolBar />}
-                data={data}
-                employee={employee}
-            />
+            <CustomTable CustomToolbar={<EmployeeToolBar />} data={data} />
             <InformationContainer>
                 <CustomPieChart data={data} />
                 <EmployeeStats data={data} />
