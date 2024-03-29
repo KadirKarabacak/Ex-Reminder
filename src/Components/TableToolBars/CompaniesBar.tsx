452a14cba8dde19f4b8ba8388bc98b0769a5ce55
Kadir <kadirht@hotmail.com>
2024-03-29 05:40:54 +0300

    feat: add make sale feature

--- src/Components/TableToolBars/CompaniesBar.tsx
+++ src/Components/TableToolBars/CompaniesBar.tsx
@@ -3,6 +3,7 @@ import { useTranslation } from "react-i18next";
 import { useState } from "react";
 import styled from "styled-components";
 import AddCompanyModal from "../Modals/AddCompanyModal";
+import AddSaleModal from "../Modals/AddSaleModal";
 
 const StyledToolBar = styled(Toolbar)`
     border-top-left-radius: 5px;
@@ -31,7 +32,7 @@ const StyledTextField = styled(TextField)`
         border-color: var(--color-grey-500);
     }
     &:hover > div > fieldset {
-        border-color: var(--color-brand-600) !important;
+        border-color: var(--color-green-new) !important;
     }
 `;
 
@@ -44,8 +45,11 @@ export function CompaniesToolBar({
 }) {
     const { t } = useTranslation();
     const [open, setOpen] = useState(false);
+    const [openMakeSale, setOpenMakeSale] = useState(false);
     const handleOpen = () => setOpen(true);
     const handleClose = () => setOpen(false);
+    const handleOpenMakeSale = () => setOpenMakeSale(true);
+    const handleCloseMakeSale = () => setOpenMakeSale(false);
 
     return (
         <>
@@ -53,15 +57,16 @@ export function CompaniesToolBar({
                 sx={{
                     pl: { sm: 3 },
                     pr: { xs: 1, sm: 2 },
-                    gap: "1rem",
+                    gap: "1.5rem",
                 }}
             >
                 <Typography
                     sx={{
                         marginRight: "auto",
-                        color: "var(--color-indigo-700)",
-                        fontSize: "2rem",
+                        color: "var(--color-green-lighter)",
+                        fontSize: "2.4rem",
                         fontWeight: "bold",
+                        borderBottom: "3px solid var(--color-green-new)",
                     }}
                     variant="subtitle1"
                     component="div"
@@ -80,9 +85,8 @@ export function CompaniesToolBar({
                         color: "var(--color-grey-50)",
                         transition: "all .3s",
                         padding: "1rem 2rem",
-                        fontSize: "1rem",
+                        fontSize: "1.1rem",
                         alignSelf: "center",
-                        fontWeight: "bold",
                         "&:hover": {
                             backgroundColor: "var(--color-grey-600)",
                             color: "var(--color-grey-100)",
@@ -99,13 +103,36 @@ export function CompaniesToolBar({
                 >
                     {t("Add Company")}
                 </Button>
-                {/* <Tooltip title="Filter list">
-                    <StyledIconButton>
-                        <FilterListIcon />
-                    </StyledIconButton>
-                </Tooltip> */}
+                <Button
+                    onClick={handleOpenMakeSale}
+                    sx={{
+                        backgroundColor: "var(--color-green-lighter)",
+                        color: "white",
+                        transition: "all .3s",
+                        padding: "1rem 2rem",
+                        fontSize: "1.1rem",
+                        alignSelf: "center",
+                        "&:hover": {
+                            backgroundColor: "var(--color-green-new)",
+                            transform: "translateY(-2px)",
+                        },
+                        "&:active": {
+                            transform: "translateY(0)",
+                        },
+                        "&:disabled": {
+                            backgroundColor: "var(--color-grey-500)",
+                        },
+                    }}
+                    variant="contained"
+                >
+                    {t("Make Sale")}
+                </Button>
             </StyledToolBar>
             <AddCompanyModal handleClose={handleClose} open={open} />
+            <AddSaleModal
+                handleClose={handleCloseMakeSale}
+                open={openMakeSale}
+            />
         </>
     );
 }
