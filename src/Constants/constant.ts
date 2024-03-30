261576f25585884c17854fd848661657e6ae38c9
Kadir <kadirht@hotmail.com>
2024-03-30 20:12:27 +0300

    fix: fix page animations

--- src/Constants/constant.ts
+++ src/Constants/constant.ts
@@ -5,8 +5,21 @@ export const springOptions = {
     from: { opacity: 0, transform: "translateY(50px)", scale: 0.9 },
     to: { opacity: 1, transform: "translateY(0)", scale: 1 },
     config: {
-        duration: 600,
+        duration: 800,
+        easing: easings.easeInOutBack,
+
+        // easing: easings.easeInOutCirc,
+    },
+};
+
+export const tableRowSpringOptions = {
+    from: { opacity: 0, transform: "translateY(15px)" },
+    to: { opacity: 1, transform: "translateY(0)" },
+    leave: { opacity: 0, transform: "translateY(-20px)" },
+    config: {
+        duration: 500,
         easing: easings.easeInOutCirc,
     },
 };
+
 export const animationDuration = "2s";
