12e8eeee128225a7e36c41aab0da36d928be25e0
Kadir <kadirht@hotmail.com>
2024-03-30 20:05:24 +0300

    feat: add accounting detail section

---
+++ src/Components/AccountingDetails.tsx
@@ -0,0 +1,31 @@
+import { Paper } from "@mui/material";
+import { Box } from "@mui/system";
+
+export default function AccountingDetails() {
+    return (
+        <Box
+            sx={{
+                width: "100%",
+                display: "flex",
+                justifyContent: "center",
+                paddingTop: "2rem",
+            }}
+        >
+            <Paper
+                sx={{
+                    width: "100%",
+                    mb: 2,
+                    backgroundColor: "transparent",
+                    maxWidth: "100%",
+                    height: "max-content",
+                    boxShadow: "var(--shadow-md)",
+                    p: "2rem",
+                    display: "flex",
+                    flexDirection: "column",
+                }}
+            >
+                Accounting Details
+            </Paper>
+        </Box>
+    );
+}
