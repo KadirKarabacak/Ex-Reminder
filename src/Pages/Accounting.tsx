b4f4b3620cbfbde4e3fd84f2c88b251d7ebdd802
Kadir <kadirht@hotmail.com>
2024-03-30 20:12:55 +0300

    feat: update accounting page

--- src/Pages/Accounting.tsx
+++ src/Pages/Accounting.tsx
@@ -3,7 +3,13 @@ import styled from "styled-components";
 import { springOptions } from "../Constants/constant";
 import { Helmet } from "react-helmet";
 import { useTranslation } from "react-i18next";
+import { useGetAccounting } from "../Api/accountingController";
+import CustomTable from "../Components/Table";
 import { useState } from "react";
+import { AccountingToolBar } from "../Components/TableToolBars/AccountingBar";
+import { InfinitySpin } from "react-loader-spinner";
+import Chart from "../Components/Chart";
+import AccountingDetails from "../Components/AccountingDetails";
 
 const StyledAccounting = styled.main`
     width: 100%;
@@ -17,35 +23,46 @@ const StyledAccounting = styled.main`
     box-shadow: var(--shadow-sm);
 `;
 
-// const FullPage = styled.div`
-//     height: 65rem;
-//     background-color: var(--color-grey-50);
-//     display: flex;
-//     align-items: center;
-//     justify-content: center;
-// `;
+const FullPage = styled.div`
+    height: 65rem;
+    background-color: var(--color-grey-50);
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`;
 
 const AnimatedStyledAccounting = animated(StyledAccounting);
 
 export default function Accounting() {
-    const animationProps = useSpring(springOptions);
-    // const [searchText, setSearchText] = useState("");
-
     const { t } = useTranslation();
+    const animationProps = useSpring(springOptions);
+    const [searchText, setSearchText] = useState("");
+    const { data, isLoading } = useGetAccounting();
 
-    // if (isLoading)
-    //     return (
-    //         <FullPage>
-    //             <InfinitySpin color="var(--color-grey-800)" />
-    //         </FullPage>
-    //     );
+    if (isLoading)
+        return (
+            <FullPage>
+                <InfinitySpin color="var(--color-grey-800)" />
+            </FullPage>
+        );
 
     return (
         <AnimatedStyledAccounting style={animationProps}>
             <Helmet>
                 <title>Ex Reminder | {t("Accounting")}</title>
             </Helmet>
-            Accounting
+            <CustomTable
+                CustomToolbar={
+                    <AccountingToolBar
+                        searchText={searchText}
+                        setSearchText={setSearchText}
+                    />
+                }
+                data={data}
+                searchText={searchText}
+            />
+            <Chart data={data} />
+            <AccountingDetails />
         </AnimatedStyledAccounting>
     );
 }
