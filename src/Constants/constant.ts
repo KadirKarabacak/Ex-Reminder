111247a1474ed8d522df6bef91668a3ac5f7a454
Kadir <kadirht@hotmail.com>
2024-04-02 23:56:51 +0300

    feat: add constants

--- src/Constants/constant.ts
+++ src/Constants/constant.ts
@@ -12,15 +12,17 @@ export const springOptions = {
     },
 };
 
-export const tableRowSpringOptions = {
+export const mapSpringOptions = {
     from: { opacity: 0, transform: "translateY(15px)" },
     to: { opacity: 1, transform: "translateY(0)" },
     leave: { opacity: 0, transform: "translateY(-20px)" },
     config: {
         duration: 500,
-        easing: easings.easeInOutCirc,
+        // easing: easings.easeOutCubic,
+        easing: easings.easeInOutQuad,
     },
 };
 
 export const animationDuration = "2s";
 export const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
+export const API_KEY = "660af325eae2a991411459bkz0d7f18";
