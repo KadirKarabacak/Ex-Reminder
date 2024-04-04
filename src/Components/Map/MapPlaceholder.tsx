a5102c6c98e094a14f876a543040319f962418a8
Kadir <kadirht@hotmail.com>
2024-04-04 16:43:38 +0300

    feat: map placeholder

---
+++ src/Components/Map/MapPlaceholder.tsx
@@ -0,0 +1,8 @@
+export default function MapPlaceholder() {
+    return (
+        <p>
+            Map of Türkiye.{" "}
+            <noscript>You need to enable JavaScript to see this map.</noscript>
+        </p>
+    );
+}
