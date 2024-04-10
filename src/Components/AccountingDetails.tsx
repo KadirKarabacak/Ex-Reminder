8e8edd39d708708f4bf5306f0b56e26d824b42aa
Kadir <kadirht@hotmail.com>
2024-04-10 03:18:02 +0300

    feat: add accounting details

--- src/Components/AccountingDetails.tsx
+++ src/Components/AccountingDetails.tsx
@@ -1,8 +1,45 @@
-import { Paper } from "@mui/material";
+import { Grid, Paper, Typography } from "@mui/material";
 import { Box } from "@mui/system";
+import { useTranslation } from "react-i18next";
+import styled from "styled-components";
+import { Sales } from "../Interfaces/User";
+import { formatCurrency } from "../Utils/utils";
+
+const StyledTitle = styled.h3`
+    color: var(--color-green-lighter);
+    align-self: center;
+    margin-bottom: 0.9rem;
+    border-bottom: 3px solid var(--color-grey-400);
+    padding-bottom: 1rem;
+`;
+
+const StyledDescription = styled.h4`
+    color: var(--color-grey-600);
+    align-self: center;
+    margin-bottom: 0.9rem;
+`;
 
 export default function AccountingDetails({ data }: { data: any }) {
-    console.log(data);
+    const { t } = useTranslation();
+
+    const totalSoldItem =
+        data &&
+        data.reduce(
+            (acc: number, sale: Sales) => (acc += +sale.saleItemAmount),
+            0
+        );
+    const totalSalePrice =
+        data &&
+        data.reduce(
+            (acc: number, sale: Sales) => (acc += sale.totalSalePrice),
+            0
+        );
+    const companiesSoldTo =
+        data &&
+        [...new Set(data.map((sale: Sales) => sale.saleCompanyName))].join(
+            ", "
+        );
+
     return (
         <Box
             sx={{
@@ -25,7 +62,45 @@ export default function AccountingDetails({ data }: { data: any }) {
                     flexDirection: "column",
                 }}
             >
-                Accounting Details
+                <Typography
+                    id="transition-modal-title"
+                    variant="h3"
+                    component="h1"
+                    sx={{
+                        fontWeight: "bold",
+                        letterSpacing: "0.80px",
+                        alignSelf: "flex-start",
+                        padding: "1rem 0 1rem 0",
+                        color: "var(--color-grey-800)",
+                        borderBottom: "3px solid var(--color-grey-500)",
+                    }}
+                >
+                    {t(`Accounting Stats`)}
+                </Typography>
+                <Grid container spacing={2} sx={{ marginTop: "1rem" }}>
+                    <Grid item xs={4}>
+                        <StyledTitle>{t("Total Sale")}</StyledTitle>
+                        <StyledDescription>
+                            {data?.length} {t("sale done")}
+                        </StyledDescription>
+                    </Grid>
+                    <Grid item xs={4}>
+                        <StyledTitle>{t("Total Sold Item")}</StyledTitle>
+                        <StyledDescription>
+                            {totalSoldItem} {t("items sold")}
+                        </StyledDescription>
+                    </Grid>
+                    <Grid item xs={4}>
+                        <StyledTitle>{t("Total Sale Price")}</StyledTitle>
+                        <StyledDescription>
+                            {formatCurrency(totalSalePrice)}
+                        </StyledDescription>
+                    </Grid>
+                    <Grid item xs={4}>
+                        <StyledTitle>{t("Companies sold to")}</StyledTitle>
+                        <StyledDescription>{companiesSoldTo}</StyledDescription>
+                    </Grid>
+                </Grid>
             </Paper>
         </Box>
     );
