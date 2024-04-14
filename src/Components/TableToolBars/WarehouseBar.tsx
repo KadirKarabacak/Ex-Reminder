7a42d8536d0ef451ef227e13a3f3f2c52c184a55
Kadir <kadirht@hotmail.com>
2024-04-14 22:26:07 +0300

    feat: add export button for whole table

--- src/Components/TableToolBars/WarehouseBar.tsx
+++ src/Components/TableToolBars/WarehouseBar.tsx
@@ -3,18 +3,44 @@ import { useTranslation } from "react-i18next";
 import { useState } from "react";
 import AddItemModal from "../Modals/Warehouses/AddItemModal";
 import SearchInput from "../SearchInput";
+import ExportButton from "../ExportButton";
+import { Warehouses } from "../../Interfaces/User";
+import { formatCurrency } from "../../Utils/utils";
 
 export function WarehouseToolBar({
     searchText,
     setSearchText,
+    data,
 }: {
     searchText: any;
     setSearchText: any;
+    data: any;
 }) {
     const { t } = useTranslation();
     const [open, setOpen] = useState(false);
     const handleOpen = () => setOpen(true);
     const handleClose = () => setOpen(false);
+
+    const ExcelData = data.map((value: Warehouses) => {
+        return {
+            itemName: value?.itemName || t("-"),
+            itemAmount: value?.itemAmount || t("-"),
+            itemSalePrice: formatCurrency(value?.itemSalePrice) || t("-"),
+            itemPurchasePrice:
+                formatCurrency(value?.itemPurchasePrice) || t("-"),
+            itemDescription: value?.itemDescription || t("-"),
+        };
+    });
+
+    const PdfBody = data.map((value: Warehouses) => {
+        return [
+            value?.itemName || t("-"),
+            value?.itemAmount || t("-"),
+            formatCurrency(value?.itemSalePrice) || t("-"),
+            formatCurrency(value?.itemPurchasePrice) || t("-"),
+            value?.itemDescription || t("-"),
+        ];
+    });
 
     return (
         <>
@@ -42,6 +68,46 @@ export function WarehouseToolBar({
                     searchText={searchText}
                     setSearchText={setSearchText}
                     label={t("Search Item by Name")}
+                />
+                <ExportButton
+                    title={t("Warehouses")}
+                    excel={{
+                        headers: [
+                            {
+                                label: t("Item Name"),
+                                key: "itemName",
+                            },
+                            {
+                                label: t("Item Amount"),
+                                key: "itemAmount",
+                            },
+                            {
+                                label: t("Item Sale Price"),
+                                key: "itemSalePrice",
+                            },
+                            {
+                                label: t("Item Purchase Price"),
+                                key: "itemPurchasePrice",
+                            },
+                            {
+                                label: t("Description"),
+                                key: "itemDescription",
+                            },
+                        ],
+                        data: ExcelData,
+                    }}
+                    pdf={{
+                        head: [
+                            [
+                                t("Item Name"),
+                                t("Item Amount"),
+                                t("Item Sale Price"),
+                                t("Item Purchase Price"),
+                                t("Description"),
+                            ],
+                        ],
+                        body: PdfBody,
+                    }}
                 />
                 <Button
                     onClick={handleOpen}
