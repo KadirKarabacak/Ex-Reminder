3c164f0216c6776300e7d0d567aad345d8d5cf25
Kadir <kadirht@hotmail.com>
2024-04-21 22:22:25 +0300

    feat: add read-unread notifications

--- src/Components/TableToolBars/NotificationBar.tsx
+++ src/Components/TableToolBars/NotificationBar.tsx
@@ -1,21 +1,95 @@
-import { Button, Toolbar, Typography } from "@mui/material";
+import {
+    Button,
+    FormControl,
+    MenuItem,
+    Select,
+    Toolbar,
+    Typography,
+} from "@mui/material";
 import { useTranslation } from "react-i18next";
 import styled from "styled-components";
 import SearchInput from "../SearchInput";
+import {
+    useGetNotifications,
+    useUpdateNotification,
+} from "../../Api/notificationController";
+import { auth } from "../../Api/firebase";
+import toast from "react-hot-toast";
+import { useSearchParams } from "react-router-dom";
 
 const StyledToolBar = styled(Toolbar)`
     border-top-left-radius: 5px;
     border-top-right-radius: 5px;
 `;
 
+const StyledSelect = styled(Select)`
+    && {
+        font-size: 1.3rem;
+        color: var(--color-grey-800);
+
+        & > fieldset {
+            border-color: var(--color-grey-500);
+        }
+
+        &:hover > fieldset {
+            border-color: var(--color-brand-600) !important;
+        }
+
+        & > .Mui-disabled {
+            background: var(--color-grey-200);
+            color: var(--color-grey-600);
+            -webkit-text-fill-color: var(--color-grey-500);
+            cursor: not-allowed;
+        }
+    }
+`;
+
 export function NotificationToolBar({
     searchText,
     setSearchText,
+    selected,
+    isAllSelected,
+    setSelected,
 }: {
-    searchText: any;
+    searchText: string;
     setSearchText: any;
+    selected: readonly string[];
+    isAllSelected: boolean;
+    setSelected: any;
 }) {
     const { t } = useTranslation();
+    const { data: notifications } = useGetNotifications();
+    const { mutateAsync: updateNotification } = useUpdateNotification();
+    const [searchParams, setSearchParams] = useSearchParams();
+    const { currentUser } = auth;
+    const userId = currentUser?.uid;
+
+    const readedOrNotReadedNotification = [
+        {
+            label: t("Unread Notifications"),
+            value: 0,
+        },
+        {
+            label: t("Notifications read"),
+            value: 1,
+        },
+    ];
+
+    async function findAndUpdateNotification() {
+        const findedNotifications = notifications?.filter(notification =>
+            selected.includes(notification.id as string)
+        );
+        findedNotifications?.map(note =>
+            updateNotification({ id: note.id, notification: note, userId })
+        );
+        setSelected([]);
+        if (searchParams.has("action", "readed"))
+            return toast.success(t(`The notifications were marked as unread`));
+        else {
+            return toast.success(t(`The notifications were marked as read`));
+        }
+    }
+    console.log(searchParams.has("action", "not-readed"));
 
     return (
         <>
@@ -44,7 +118,46 @@ export function NotificationToolBar({
                     setSearchText={setSearchText}
                     label={t("Search Notification by date")}
                 />
+                <FormControl sx={{ minWidth: "10%" }}>
+                    <StyledSelect
+                        sx={{
+                            "& > div": {
+                                padding: "12.5px 14px",
+                            },
+                        }}
+                        labelId="selectedNotificationLabel"
+                        id="selectedNotification"
+                        value={
+                            searchParams.get("action") === "not-readed" ? 0 : 1
+                        }
+                        onChange={e => {
+                            searchParams.set(
+                                "action",
+                                e.target.value === 0 ? "not-readed" : "readed"
+                            );
+                            setSearchParams(searchParams);
+                            setSelected([]);
+                        }}
+                    >
+                        {readedOrNotReadedNotification.map(
+                            (notificationType, i) => (
+                                <MenuItem
+                                    sx={{
+                                        borderBottom: "1px solid #efefef",
+                                    }}
+                                    disableRipple
+                                    key={i}
+                                    value={notificationType.value}
+                                >
+                                    {notificationType.label}
+                                </MenuItem>
+                            )
+                        )}
+                    </StyledSelect>
+                </FormControl>
                 <Button
+                    onClick={findAndUpdateNotification}
+                    disabled={selected.length === 0}
                     sx={{
                         backgroundColor: "var(--color-grey-800)",
                         color: "var(--color-grey-50)",
@@ -66,7 +179,14 @@ export function NotificationToolBar({
                     }}
                     variant="contained"
                 >
-                    {t("Mark as read")}
+                    {isAllSelected && searchParams.has("action", "not-readed")
+                        ? t("Mark all as read")
+                        : isAllSelected && searchParams.has("action", "readed")
+                        ? t("Mark all as unread")
+                        : !isAllSelected &&
+                          searchParams.has("action", "not-readed")
+                        ? t("Mark as read")
+                        : t("Mark as unread")}
                 </Button>
             </StyledToolBar>
         </>
