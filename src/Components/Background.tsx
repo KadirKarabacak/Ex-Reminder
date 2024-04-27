e85c59b20fd086c9e85d83ce7c826488513de54c
Kadir <kadirht@hotmail.com>
2024-04-27 18:56:51 +0300

    feat: add animation to forms

---
+++ src/Components/Background.tsx
@@ -0,0 +1,72 @@
+import { AnimeInstance } from "animejs";
+import anime from "animejs/lib/anime.es.js";
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+
+export default function Background() {
+    const [animationKey, setAnimationKey] = useState(0);
+    const { key } = useLocation();
+
+    useEffect(() => {
+        // Her route değiştiğinde animationKey state'ini artırarak animasyonu yeniden başlat
+        setAnimationKey(prevKey => prevKey + 1);
+    }, [key]);
+
+    useEffect(() => {
+        let animationInstance: AnimeInstance | undefined;
+
+        function startAnimation() {
+            animationInstance = anime({
+                targets: ".square, .circle, .triangle",
+                translateX: function () {
+                    return anime.random(-550, 550);
+                },
+                translateY: function () {
+                    return anime.random(-270, 270);
+                },
+                rotate: function () {
+                    return anime.random(0, 360);
+                },
+                scale: function () {
+                    return anime.random(0.5, 2);
+                },
+
+                duration: 5000,
+                easing: "easeInOutSine",
+                complete: startAnimation,
+            });
+        }
+
+        // Animasyonu başlat
+        startAnimation();
+
+        // Cleanup işlemi: Component unmount olduğunda animasyonu durdur
+        return () => {
+            if (animationInstance) {
+                animationInstance.pause();
+            }
+        };
+    }, [animationKey]);
+
+    return (
+        <div>
+            <div className="square"></div>
+            <div className="square"></div>
+            <div className="square"></div>
+            <div className="square"></div>
+            <div className="square"></div>
+
+            <div className="circle"></div>
+            <div className="circle"></div>
+            <div className="circle"></div>
+            <div className="circle"></div>
+            <div className="circle"></div>
+
+            <div className="triangle"></div>
+            <div className="triangle"></div>
+            <div className="triangle"></div>
+            <div className="triangle"></div>
+            <div className="triangle"></div>
+        </div>
+    );
+}
