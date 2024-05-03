f99bcf24960aa3eeda56c03f8e0709a22b1143ac
Kadir <kadirht@hotmail.com>
2024-05-03 18:44:07 +0300

    feat: add joyride localstorage state & refactor companies page

--- src/Pages/Companies.tsx
+++ src/Pages/Companies.tsx
@@ -1,17 +1,24 @@
 import styled from "styled-components";
-import { animated, useSpring } from "react-spring";
-import { springOptions } from "../Constants/constant";
+import { animated, easings, useSpring } from "react-spring";
 import { Helmet } from "react-helmet";
 import { useTranslation } from "react-i18next";
 import CustomTable from "../Components/Table";
 import { CompaniesToolBar } from "../Components/TableToolBars/CompaniesBar";
 import { useGetCompanies } from "../Api/companyController";
 import { InfinitySpin } from "react-loader-spinner";
-import { useParams } from "react-router-dom";
-import { OperationsHeader } from "../Components/CompanyOperations/OperationsHeader";
+import { useLocation, useParams } from "react-router-dom";
 import AnimatedPage from "../Components/AnimatedPage";
-import OperationsBody from "../Components/CompanyOperations/OperationsBody";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import i18n from "../i18n";
+import JoyrideTitle from "../Components/JoyrideTitle";
+import { CallBackProps } from "react-joyride";
+import CustomJoyride from "../Components/CustomJoyride";
+import DomainAddIcon from "@mui/icons-material/DomainAdd";
+import FileDownloadIcon from "@mui/icons-material/FileDownload";
+import SearchIcon from "@mui/icons-material/Search";
+import SettingsApplicationsIcon from "@mui/icons-material/SettingsApplications";
+import PostAddIcon from "@mui/icons-material/PostAdd";
+import CompanyOperations from "./CompanyOperations";
 
 const StyledCompany = styled.main`
     width: 100%;
@@ -33,32 +40,121 @@ const FullPage = styled.div`
     justify-content: center;
 `;
 
-const StyledOperations = styled.main`
-    width: 100%;
-    min-height: 60rem;
-    background-color: var(--color-grey-100);
-    display: flex;
-    justify-content: flex-start;
-    align-items: flex-start;
-    padding: 2rem 2rem;
-    border-radius: var(--border-radius-md);
-    box-shadow: var(--shadow-sm);
-`;
+const iconStyle = {
+    width: "2rem",
+    height: "2rem",
+    color: "var(--color-grey-300)",
+    transition: "all .3s",
+};
+
+const companiesSteps = [
+    {
+        target: ".makeSaleFromCompany-btn",
+        content: i18n.t(
+            "Here you can create a sales record for a company in your table. Sales records are recorded both in the operations section of the company and in the accounting section of your application."
+        ),
+        placement: "bottom-end",
+        title: (
+            <JoyrideTitle
+                icon={<PostAddIcon sx={iconStyle} />}
+                title={i18n.t("Make Sales")}
+            />
+        ),
+    },
+    {
+        target: ".addCompany-btn",
+        content: i18n.t(
+            "Here you can add and list the companies you do business with in your companies table."
+        ),
+        placement: "bottom-end",
+        title: (
+            <JoyrideTitle
+                icon={<DomainAddIcon sx={iconStyle} />}
+                title={i18n.t("Add Companies")}
+            />
+        ),
+    },
+    {
+        target: ".export-btn-companies",
+        content: i18n.t(
+            "Here you can print out all companies data in your table as PDF or Excel."
+        ),
+        placement: "bottom-end",
+        title: (
+            <JoyrideTitle
+                icon={<FileDownloadIcon sx={iconStyle} />}
+                title={i18n.t("Export Data")}
+            />
+        ),
+    },
+    {
+        target: ".search-input-companies",
+        content: i18n.t(
+            "From here, you can find a company in your table much more easily by searching by company name."
+        ),
+        placement: "bottom-end",
+        title: (
+            <JoyrideTitle
+                icon={<SearchIcon sx={iconStyle} />}
+                title={i18n.t("Search Data")}
+            />
+        ),
+    },
+    {
+        target: ".button-group-companies",
+        content: i18n.t(
+            "From here, you can edit, delete, add agreements, negotiations and sales to your companies. You can also browse the operations section for more details about agreements, sales and the company."
+        ),
+        placement: "bottom-end",
+        title: (
+            <JoyrideTitle
+                icon={<SettingsApplicationsIcon sx={iconStyle} />}
+                title={i18n.t("Company Operations")}
+            />
+        ),
+    },
+];
 
 const AnimatedStyledCompany = animated(StyledCompany);
-const AnimatedStyledOperations = animated(StyledOperations);
 
 export default function Companies() {
-    const animationProps = useSpring(springOptions);
+    const [isAnimationEnd, setIsAnimationEnd] = useState(false);
+    const animationProps = useSpring({
+        from: { opacity: 0, transform: "translateY(50px)" },
+        to: { opacity: 1, transform: "translateY(0)" },
+        config: {
+            duration: 800,
+            easing: easings.easeInOutBack,
+        },
+        onRest: () => setIsAnimationEnd(true),
+    });
     const { t } = useTranslation();
+    const { pathname } = useLocation();
     const { data, isLoading } = useGetCompanies();
     const [searchText, setSearchText] = useState("");
     const [selected, setSelected] = useState<readonly number[]>([]);
+    const [runJoyride, setRunJoyride] = useState(false);
     const params = useParams();
     const { companyId } = params;
-
     const currentCompany =
         companyId && data?.find(comp => comp.id === companyId);
+
+    useEffect(() => {
+        if (localStorage.getItem("isCompaniesJoyrideDisplayed") === "true")
+            return;
+        else {
+            localStorage.setItem("isCompaniesJoyrideDisplayed", "false");
+            isAnimationEnd && setRunJoyride(true);
+        }
+    }, [isAnimationEnd]);
+
+    const handleJoyrideCallback = (data: CallBackProps) => {
+        const { status } = data;
+        if (status === "finished") {
+            localStorage.setItem("isCompaniesJoyrideDisplayed", "true");
+            setRunJoyride(false);
+        }
+    };
 
     if (isLoading)
         return (
@@ -67,20 +163,7 @@ export default function Companies() {
             </FullPage>
         );
 
-    if (companyId)
-        return (
-            <AnimatedStyledOperations style={animationProps}>
-                <AnimatedPage>
-                    <>
-                        <Helmet>
-                            <title>Ex Reminder | {t("Operations")}</title>
-                        </Helmet>
-                        <OperationsHeader currentCompany={currentCompany} />
-                        <OperationsBody currentCompany={currentCompany} />
-                    </>
-                </AnimatedPage>
-            </AnimatedStyledOperations>
-        );
+    if (companyId) return <CompanyOperations currentCompany={currentCompany} />;
 
     if (!companyId)
         return (
@@ -103,6 +186,11 @@ export default function Companies() {
                         selected={selected}
                     />
                 </AnimatedPage>
+                <CustomJoyride
+                    pathname={runJoyride}
+                    callback={handleJoyrideCallback}
+                    steps={companiesSteps}
+                />
             </AnimatedStyledCompany>
         );
 }
