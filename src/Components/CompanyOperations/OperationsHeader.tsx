d6e03001894c7eaf663d0244b875ef24561c4a3c
Kadir <kadirht@hotmail.com>
2024-05-17 21:55:22 +0300

    feat: add responsive styles

--- src/Components/CompanyOperations/OperationsHeader.tsx
+++ src/Components/CompanyOperations/OperationsHeader.tsx
@@ -1,10 +1,11 @@
-import { Button, Toolbar, Typography } from "@mui/material";
+import { Button, Menu, MenuItem, Toolbar, Typography } from "@mui/material";
 import { useTranslation } from "react-i18next";
 import styled from "styled-components";
 import { useNavigate } from "react-router-dom";
 import { useState } from "react";
 import AddAgreementModal from "../Modals/Agreements/AddAgreementModal";
 import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
 
 const StyledToolBar = styled(Toolbar)`
     border-top-left-radius: 5px;
@@ -17,11 +18,34 @@ const StyledSpan = styled.span`
     padding-left: 8px;
 `;
 
+const StyledSmallContainer = styled.div`
+    display: none;
+    @media (max-width: 1000px) {
+        display: block;
+    }
+`;
+
+const StyledLargeContainer = styled.div`
+    display: flex;
+    gap: 1rem;
+    @media (max-width: 1000px) {
+        display: none;
+    }
+`;
+
 export function OperationsHeader({ currentCompany }: { currentCompany: any }) {
     const { t } = useTranslation();
     const navigate = useNavigate();
     const [opensAgreement, setOpensAgreement] = useState(false);
+    const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+    const opensMenu = Boolean(anchorEl);
 
+    const handleClickMenuItem = (event: React.MouseEvent<HTMLElement>) => {
+        setAnchorEl(event.currentTarget);
+    };
+    const handleCloseMenu = () => {
+        setAnchorEl(null);
+    };
     const handleOpenAgreement = () => setOpensAgreement(true);
     const handleCloseAgreement = () => setOpensAgreement(false);
 
@@ -31,6 +55,9 @@ export function OperationsHeader({ currentCompany }: { currentCompany: any }) {
                 sx={{
                     pl: { sm: 3 },
                     pr: { xs: 1, sm: 2 },
+                    "@media (max-width: 700px)": {
+                        padding: "0.2rem",
+                    },
                     gap: "1rem",
                     width: "100%",
                 }}
@@ -49,58 +76,162 @@ export function OperationsHeader({ currentCompany }: { currentCompany: any }) {
                     {t("Operations")}{" "}
                     <StyledSpan> {currentCompany?.companyName}</StyledSpan>
                 </Typography>
-                <Button
-                    onClick={handleOpenAgreement}
-                    sx={{
-                        backgroundColor: "var(--color-green-lighter)",
-                        color: "#f5f3f3",
-                        transition: "all .3s",
-                        padding: "1rem 2rem",
-                        fontSize: "1.1rem",
-                        alignSelf: "center",
-                        fontWeight: "bold",
-                        "&:hover": {
-                            backgroundColor: "var(--color-green-new)",
-                            transform: "translateY(-2px)",
-                        },
-                        "&:active": {
-                            transform: "translateY(0)",
-                        },
-                        "&:disabled": {
-                            backgroundColor: "var(--color-grey-500)",
-                        },
-                    }}
-                    variant="contained"
-                >
-                    {t("Add Agreement")}
-                </Button>
+                <StyledSmallContainer>
+                    <Button
+                        sx={{
+                            backgroundColor: "var(--color-grey-800)",
+                            color: "var(--color-grey-50)",
+                            transition: "all .3s",
+                            padding: "1rem 2rem",
+                            fontSize: "1.1rem",
+                            alignSelf: "center",
+                            "&:hover": {
+                                backgroundColor: "var(--color-grey-600)",
+                                color: "var(--color-grey-100)",
+                            },
+                            "&:disabled": {
+                                backgroundColor: "var(--color-grey-500)",
+                            },
+                            "& > span > svg": { fill: "var(--color-grey-50)" },
+                        }}
+                        id="demo-customized-button"
+                        aria-controls={
+                            opensMenu ? "demo-customized-menu" : undefined
+                        }
+                        aria-haspopup="true"
+                        aria-expanded={opensMenu ? "true" : undefined}
+                        variant="contained"
+                        disableElevation
+                        onClick={handleClickMenuItem}
+                        endIcon={<KeyboardArrowDownIcon />}
+                    >
+                        Operations
+                    </Button>
+                    <Menu
+                        id="demo-customized-menu"
+                        MenuListProps={{
+                            "aria-labelledby": "demo-customized-button",
+                        }}
+                        anchorEl={anchorEl}
+                        open={opensMenu}
+                        onClose={handleCloseMenu}
+                    >
+                        <MenuItem disableRipple>
+                            <Button
+                                onClick={handleOpenAgreement}
+                                sx={{
+                                    backgroundColor:
+                                        "var(--color-green-lighter)",
+                                    color: "#f5f3f3",
+                                    transition: "all .3s",
+                                    padding: "1rem 2rem",
+                                    fontSize: "1.1rem",
+                                    alignSelf: "center",
+                                    fontWeight: "bold",
+                                    width: "100%",
+                                    "&:hover": {
+                                        backgroundColor:
+                                            "var(--color-green-new)",
+                                        transform: "translateY(-2px)",
+                                    },
+                                    "&:active": {
+                                        transform: "translateY(0)",
+                                    },
+                                    "&:disabled": {
+                                        backgroundColor:
+                                            "var(--color-grey-500)",
+                                    },
+                                }}
+                                variant="contained"
+                            >
+                                {t("Add Agreement")}
+                            </Button>
+                        </MenuItem>
+                        <MenuItem onClick={handleCloseMenu} disableRipple>
+                            <Button
+                                onClick={() => navigate("/companies")}
+                                sx={{
+                                    color: "var(--color-grey-800)",
+                                    transition: "all .3s",
+                                    padding: "1rem 1.6rem",
+                                    fontSize: "1.1rem",
+                                    border: "1px solid var(--color-grey-500)",
+                                    backgroundColor: "var(--color-grey-100)",
+                                    fontWeight: "bold",
+                                    display: "flex",
+                                    alignItems: "center",
+                                    gap: "5px",
+                                    "&:hover": {
+                                        backgroundColor:
+                                            "var(--color-grey-200)",
+                                        transform: "translateY(-2px)",
+                                        border: "1px solid var(--color-grey-800)",
+                                    },
+                                    "&:active": {
+                                        transform: "translateY(0)",
+                                    },
+                                }}
+                                variant="outlined"
+                            >
+                                <ArrowBackIcon /> {t("Back to Companies")}
+                            </Button>
+                        </MenuItem>
+                    </Menu>
+                </StyledSmallContainer>
+                <StyledLargeContainer>
+                    <Button
+                        onClick={handleOpenAgreement}
+                        sx={{
+                            backgroundColor: "var(--color-green-lighter)",
+                            color: "#f5f3f3",
+                            transition: "all .3s",
+                            padding: "1rem 2rem",
+                            fontSize: "1.1rem",
+                            alignSelf: "center",
+                            fontWeight: "bold",
+                            "&:hover": {
+                                backgroundColor: "var(--color-green-new)",
+                                transform: "translateY(-2px)",
+                            },
+                            "&:active": {
+                                transform: "translateY(0)",
+                            },
+                            "&:disabled": {
+                                backgroundColor: "var(--color-grey-500)",
+                            },
+                        }}
+                        variant="contained"
+                    >
+                        {t("Add Agreement")}
+                    </Button>
 
-                <Button
-                    onClick={() => navigate("/companies")}
-                    sx={{
-                        color: "var(--color-grey-800)",
-                        transition: "all .3s",
-                        padding: "1rem 1.6rem",
-                        fontSize: "1.1rem",
-                        border: "1px solid var(--color-grey-500)",
-                        backgroundColor: "var(--color-grey-100)",
-                        fontWeight: "bold",
-                        display: "flex",
-                        alignItems: "center",
-                        gap: "5px",
-                        "&:hover": {
-                            backgroundColor: "var(--color-grey-200)",
-                            transform: "translateY(-2px)",
-                            border: "1px solid var(--color-grey-800)",
-                        },
-                        "&:active": {
-                            transform: "translateY(0)",
-                        },
-                    }}
-                    variant="outlined"
-                >
-                    <ArrowBackIcon /> {t("Back to Companies")}
-                </Button>
+                    <Button
+                        onClick={() => navigate("/companies")}
+                        sx={{
+                            color: "var(--color-grey-800)",
+                            transition: "all .3s",
+                            padding: "1rem 1.6rem",
+                            fontSize: "1.1rem",
+                            border: "1px solid var(--color-grey-500)",
+                            backgroundColor: "var(--color-grey-100)",
+                            fontWeight: "bold",
+                            display: "flex",
+                            alignItems: "center",
+                            gap: "5px",
+                            "&:hover": {
+                                backgroundColor: "var(--color-grey-200)",
+                                transform: "translateY(-2px)",
+                                border: "1px solid var(--color-grey-800)",
+                            },
+                            "&:active": {
+                                transform: "translateY(0)",
+                            },
+                        }}
+                        variant="outlined"
+                    >
+                        <ArrowBackIcon /> {t("Back to Companies")}
+                    </Button>
+                </StyledLargeContainer>
             </StyledToolBar>
             <AddAgreementModal
                 handleClose={handleCloseAgreement}
