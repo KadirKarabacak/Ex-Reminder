ae9711dbfa740e2398e061a8ca7fdb0fe5a7a5d6
KadirKarabacak <kadirht@hotmail.com>
2024-07-30 15:26:03 +0300

    fix: fix firebase secrets

--- src/Api/firebase.ts
+++ src/Api/firebase.ts
@@ -3,17 +3,6 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
-//: Firebase Default Setup
-// const firebaseApp = initializeApp({
-//     apiKey: "AIzaSyCTDVf4r8rsaIL1gRMnXl6tmGrC-Cl4e1w",
-//     authDomain: "my-router-project.firebaseapp.com",
-//     projectId: "my-router-project",
-//     storageBucket: "my-router-project.appspot.com",
-//     messagingSenderId: "999918257827",
-//     appId: "1:999918257827:web:d679ac1b87b845cd27a116",
-//     measurementId: "G-9H14BENC87",
-// });
-
 const firebaseConfig = {
     apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
     authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
