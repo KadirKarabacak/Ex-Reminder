6ac62b10482ac9ba465ed0d5a989d56039996c17
KadirKarabacak <kadirht@hotmail.com>
2024-08-19 22:25:17 +0300

    fix: fix visual bugs

--- src/styles/GlobalStyles.ts
+++ src/styles/GlobalStyles.ts
@@ -164,6 +164,20 @@ input:disabled, label:disabled {
   -webkit-text-fill-color: var(--color-grey-500)!important;
 }
 
+input:-webkit-autofill,
+input:-webkit-autofill:hover,
+input:-webkit-autofill:focus,
+textarea:-webkit-autofill,
+textarea:-webkit-autofill:hover,
+textarea:-webkit-autofill:focus,
+select:-webkit-autofill,
+select:-webkit-autofill:hover,
+select:-webkit-autofill:focus {
+    -webkit-text-fill-color: var(--color-grey-600); 
+    -webkit-box-shadow: 0 0 0px 1000px transparent inset; /* Arka planı şeffaf yapar */
+    transition: background-color 5000s ease-in-out 0s;
+}
+
 
 /* textarea:focus,
 select:focus {
