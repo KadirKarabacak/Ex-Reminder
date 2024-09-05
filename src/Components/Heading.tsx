6a3fc3517c162c93515a45a568c3a5f75376a9b0
KadirKarabacak <kadirht@hotmail.com>
2024-09-06 00:53:11 +0300

    fix: fix styling

--- src/Components/Heading.tsx
+++ src/Components/Heading.tsx
@@ -5,6 +5,10 @@ const StyledHeading = styled.h2`
     font-size: 3.5rem;
     color: var(--color-grey-800);
     font-weight: bold;
+
+    @media (max-width: 500px) {
+        font-size: 2.5rem;
+    }
 `;
 
 export default function Heading({ title }: HeadingProps) {
