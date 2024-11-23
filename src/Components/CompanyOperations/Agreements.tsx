d433d54b2c2ec6871300eb41e5f670dba2255c02
Kadir <kadirht@hotmail.com>
2024-11-24 02:35:20 +0300

    fix: fix agreement end date

--- src/Components/CompanyOperations/Agreements.tsx
+++ src/Components/CompanyOperations/Agreements.tsx
@@ -1,249 +1,250 @@
-import { Button, Divider, Grid } from "@mui/material";
-import { useTranslation } from "react-i18next";
-import styled from "styled-components";
-import { remainingTime } from "../../Utils/utils";
-import { useState } from "react";
-import EditAgreementModal from "../Modals/Agreements/EditAgreementModal";
-import { useSearchParams } from "react-router-dom";
-import DeleteAgreementModal from "../Modals/Agreements/DeleteAgreementModal";
+import { Button, Divider, Grid } from '@mui/material';
+import { useTranslation } from 'react-i18next';
+import styled from 'styled-components';
+import { remainingTime } from '../../Utils/utils';
+import { useState } from 'react';
+import EditAgreementModal from '../Modals/Agreements/EditAgreementModal';
+import { useSearchParams } from 'react-router-dom';
+import DeleteAgreementModal from '../Modals/Agreements/DeleteAgreementModal';
 
 const StyledDivider = styled(Divider)`
-    color: var(--color-green-lighter);
-    font-weight: bold;
+  color: var(--color-green-lighter);
+  font-weight: bold;
 
-    & > .MuiDivider-wrapper::before {
-        border-top: thin solid rgba(255, 255, 255, 1) !important;
-    }
+  & > .MuiDivider-wrapper::before {
+    border-top: thin solid rgba(255, 255, 255, 1) !important;
+  }
 `;
 
 const StyledTitle = styled.h3`
-    color: var(--color-grey-900);
-    font-size: 2rem;
-    font-weight: bold;
-    margin-bottom: 0.6rem;
+  color: var(--color-grey-900);
+  font-size: 2rem;
+  font-weight: bold;
+  margin-bottom: 0.6rem;
 `;
 
 const StyledDescription = styled.p`
-    color: var(--color-grey-500);
-    font-size: 1.4rem;
+  color: var(--color-grey-500);
+  font-size: 1.4rem;
 `;
 
 export default function Agreements({
-    agreement,
-    currentCompany,
+  agreement,
+  currentCompany,
 }: {
-    agreement: any;
-    currentCompany: any;
+  agreement: any;
+  currentCompany: any;
 }) {
-    const { t } = useTranslation();
-    const [opensEdit, setOpensEdit] = useState(false);
-    const [opensDelete, setOpensDelete] = useState(false);
-    const [searchParams, setSearchParams] = useSearchParams();
+  const { t } = useTranslation();
+  const [opensEdit, setOpensEdit] = useState(false);
+  const [opensDelete, setOpensDelete] = useState(false);
+  const [searchParams, setSearchParams] = useSearchParams();
 
-    const handleOpenEdit = () => {
-        setOpensEdit(true);
-        setSearchParams("edit-agreement");
-    };
-    const handleCloseEdit = () => {
-        setOpensEdit(false);
-        setTimeout(() => {
-            setSearchParams("");
-        }, 500);
-    };
-    const handleOpenDelete = () => {
-        setOpensDelete(true);
-        setSearchParams("delete-agreement");
-    };
-    const handleCloseDelete = () => {
-        setOpensDelete(false);
-        setTimeout(() => {
-            setSearchParams("");
-        }, 500);
-    };
+  const handleOpenEdit = () => {
+    setOpensEdit(true);
+    setSearchParams('edit-agreement');
+  };
+  const handleCloseEdit = () => {
+    setOpensEdit(false);
+    setTimeout(() => {
+      setSearchParams('');
+    }, 500);
+  };
+  const handleOpenDelete = () => {
+    setOpensDelete(true);
+    setSearchParams('delete-agreement');
+  };
+  const handleCloseDelete = () => {
+    setOpensDelete(false);
+    setTimeout(() => {
+      setSearchParams('');
+    }, 500);
+  };
 
-    return (
-        <Grid
-            container
-            spacing={2}
-            sx={{
-                width: "100%",
-                backgroundColor: "var(--color-grey-100)",
-                ":hover": {
-                    cursor: "pointer",
-                },
-                marginLeft: "0px",
-            }}
+  console.log(agreement.agreementEndDate);
+
+  return (
+    <Grid
+      container
+      spacing={2}
+      sx={{
+        width: '100%',
+        backgroundColor: 'var(--color-grey-100)',
+        ':hover': {
+          cursor: 'pointer',
+        },
+        marginLeft: '0px',
+      }}
+    >
+      <Grid
+        item
+        xs={12}
+        sx={{
+          paddingTop: '0',
+          mb: '1rem',
+        }}
+      >
+        <StyledDivider>{agreement.createdAt}</StyledDivider>
+      </Grid>
+      <Grid
+        item
+        xs={6}
+        md={4}
+        sx={{
+          border: '1px solid var(--color-grey-200)',
+          paddingBottom: '2rem',
+          borderTopLeftRadius: '5px',
+        }}
+      >
+        <StyledTitle>{t(`Agreement Content`)}</StyledTitle>
+        <StyledDescription>
+          {agreement?.agreementContent || t('Not spesified')}
+        </StyledDescription>
+      </Grid>
+      <Grid
+        item
+        xs={6}
+        md={4}
+        sx={{
+          border: '1px solid var(--color-grey-200)',
+          paddingBottom: '2rem',
+        }}
+      >
+        <StyledTitle>{t(`Agreement Budget`)}</StyledTitle>
+        <StyledDescription>
+          {agreement?.agreementBudget || t('Not spesified')}
+        </StyledDescription>
+      </Grid>
+      <Grid
+        item
+        xs={6}
+        md={4}
+        sx={{
+          border: '1px solid var(--color-grey-200)',
+          paddingBottom: '2rem',
+          borderTopRightRadius: '5px',
+        }}
+      >
+        <StyledTitle>{t(`Agreement Parties`)}</StyledTitle>
+        <StyledDescription>
+          {agreement?.agreementParties || t('Not spesified')}
+        </StyledDescription>
+      </Grid>
+      <Grid
+        item
+        xs={6}
+        md={4}
+        sx={{
+          border: '1px solid var(--color-grey-200)',
+          paddingBottom: '2rem',
+          borderBottomLeftRadius: '5px',
+        }}
+      >
+        <StyledTitle>{t(`Agreement Start Date`)}</StyledTitle>
+        <StyledDescription>
+          {agreement?.agreementStartDate || t('Not spesified')}
+        </StyledDescription>
+      </Grid>
+      <Grid
+        item
+        xs={6}
+        md={4}
+        sx={{
+          border: '1px solid var(--color-grey-200)',
+          paddingBottom: '2rem',
+        }}
+      >
+        <StyledTitle>{t(`Agreement End Date`)}</StyledTitle>
+        <StyledDescription>
+          {agreement?.agreementEndDate || t('Not spesified')}
+        </StyledDescription>
+      </Grid>
+      <Grid
+        item
+        xs={6}
+        md={4}
+        sx={{
+          border: '1px solid var(--color-grey-200)',
+          paddingBottom: '2rem',
+          borderBottomRightRadius: '5px',
+        }}
+      >
+        <StyledTitle>{t(`Agreement Remaining Time`)}</StyledTitle>
+        <StyledDescription>
+          {remainingTime(agreement.agreementEndDate) || t('Not spesified')}
+        </StyledDescription>
+      </Grid>
+      <Grid
+        item
+        xs={12}
+        sx={{
+          display: 'flex',
+          justifyContent: 'center',
+          gap: '1.5rem',
+          marginBottom: '3rem',
+        }}
+      >
+        <Button
+          onClick={handleOpenEdit}
+          sx={{
+            backgroundColor: 'var(--color-grey-800)',
+            color: 'var(--color-grey-50)',
+            transition: 'all .3s',
+            padding: '1rem 2rem',
+            fontSize: '1.1rem',
+            alignSelf: 'flex-start',
+            fontWeight: 'bold',
+            '&:hover': {
+              backgroundColor: 'var(--color-grey-700)',
+              transform: 'translateY(-2px)',
+            },
+            '&:active': {
+              transform: 'translateY(0)',
+            },
+          }}
+          variant="contained"
         >
-            <Grid
-                item
-                xs={12}
-                sx={{
-                    paddingTop: "0",
-                    mb: "1rem",
-                }}
-            >
-                <StyledDivider>{agreement.createdAt}</StyledDivider>
-            </Grid>
-            <Grid
-                item
-                xs={6}
-                md={4}
-                sx={{
-                    border: "1px solid var(--color-grey-200)",
-                    paddingBottom: "2rem",
-                    borderTopLeftRadius: "5px",
-                }}
-            >
-                <StyledTitle>{t(`Agreement Content`)}</StyledTitle>
-                <StyledDescription>
-                    {agreement?.agreementContent || t("Not spesified")}
-                </StyledDescription>
-            </Grid>
-            <Grid
-                item
-                xs={6}
-                md={4}
-                sx={{
-                    border: "1px solid var(--color-grey-200)",
-                    paddingBottom: "2rem",
-                }}
-            >
-                <StyledTitle>{t(`Agreement Budget`)}</StyledTitle>
-                <StyledDescription>
-                    {agreement?.agreementBudget || t("Not spesified")}
-                </StyledDescription>
-            </Grid>
-            <Grid
-                item
-                xs={6}
-                md={4}
-                sx={{
-                    border: "1px solid var(--color-grey-200)",
-                    paddingBottom: "2rem",
-                    borderTopRightRadius: "5px",
-                }}
-            >
-                <StyledTitle>{t(`Agreement Parties`)}</StyledTitle>
-                <StyledDescription>
-                    {agreement?.agreementParties || t("Not spesified")}
-                </StyledDescription>
-            </Grid>
-            <Grid
-                item
-                xs={6}
-                md={4}
-                sx={{
-                    border: "1px solid var(--color-grey-200)",
-                    paddingBottom: "2rem",
-                    borderBottomLeftRadius: "5px",
-                }}
-            >
-                <StyledTitle>{t(`Agreement Start Date`)}</StyledTitle>
-                <StyledDescription>
-                    {agreement?.agreementStartDate || t("Not spesified")}
-                </StyledDescription>
-            </Grid>
-            <Grid
-                item
-                xs={6}
-                md={4}
-                sx={{
-                    border: "1px solid var(--color-grey-200)",
-                    paddingBottom: "2rem",
-                }}
-            >
-                <StyledTitle>{t(`Agreement End Date`)}</StyledTitle>
-                <StyledDescription>
-                    {agreement?.agreementEndDate || t("Not spesified")}
-                </StyledDescription>
-            </Grid>
-            <Grid
-                item
-                xs={6}
-                md={4}
-                sx={{
-                    border: "1px solid var(--color-grey-200)",
-                    paddingBottom: "2rem",
-                    borderBottomRightRadius: "5px",
-                }}
-            >
-                <StyledTitle>{t(`Agreement Remaining Time`)}</StyledTitle>
-                <StyledDescription>
-                    {remainingTime(agreement.agreementEndDate) ||
-                        t("Not spesified")}
-                </StyledDescription>
-            </Grid>
-            <Grid
-                item
-                xs={12}
-                sx={{
-                    display: "flex",
-                    justifyContent: "center",
-                    gap: "1.5rem",
-                    marginBottom: "3rem",
-                }}
-            >
-                <Button
-                    onClick={handleOpenEdit}
-                    sx={{
-                        backgroundColor: "var(--color-grey-800)",
-                        color: "var(--color-grey-50)",
-                        transition: "all .3s",
-                        padding: "1rem 2rem",
-                        fontSize: "1.1rem",
-                        alignSelf: "flex-start",
-                        fontWeight: "bold",
-                        "&:hover": {
-                            backgroundColor: "var(--color-grey-700)",
-                            transform: "translateY(-2px)",
-                        },
-                        "&:active": {
-                            transform: "translateY(0)",
-                        },
-                    }}
-                    variant="contained"
-                >
-                    {t("Edit Agreement")}
-                </Button>
-                <Button
-                    onClick={handleOpenDelete}
-                    sx={{
-                        backgroundColor: "var(--color-red-700)",
-                        color: "white",
-                        transition: "all .3s",
-                        padding: "1rem 2rem",
-                        fontSize: "1.1rem",
-                        alignSelf: "flex-start",
-                        fontWeight: "bold",
-                        "&:hover": {
-                            backgroundColor: "var(--color-red-800)",
-                            transform: "translateY(-2px)",
-                        },
-                        "&:active": {
-                            transform: "translateY(0)",
-                        },
-                    }}
-                    variant="contained"
-                >
-                    {t("Delete Agreement")}
-                </Button>
-            </Grid>
-            {searchParams.has("edit-agreement") && (
-                <EditAgreementModal
-                    agreement={agreement}
-                    open={opensEdit}
-                    handleClose={handleCloseEdit}
-                    currentCompany={currentCompany}
-                />
-            )}
-            {searchParams.has("delete-agreement") && (
-                <DeleteAgreementModal
-                    agreement={agreement}
-                    open={opensDelete}
-                    handleClose={handleCloseDelete}
-                    currentCompany={currentCompany}
-                />
-            )}
-        </Grid>
-    );
+          {t('Edit Agreement')}
+        </Button>
+        <Button
+          onClick={handleOpenDelete}
+          sx={{
+            backgroundColor: 'var(--color-red-700)',
+            color: 'white',
+            transition: 'all .3s',
+            padding: '1rem 2rem',
+            fontSize: '1.1rem',
+            alignSelf: 'flex-start',
+            fontWeight: 'bold',
+            '&:hover': {
+              backgroundColor: 'var(--color-red-800)',
+              transform: 'translateY(-2px)',
+            },
+            '&:active': {
+              transform: 'translateY(0)',
+            },
+          }}
+          variant="contained"
+        >
+          {t('Delete Agreement')}
+        </Button>
+      </Grid>
+      {searchParams.has('edit-agreement') && (
+        <EditAgreementModal
+          agreement={agreement}
+          open={opensEdit}
+          handleClose={handleCloseEdit}
+          currentCompany={currentCompany}
+        />
+      )}
+      {searchParams.has('delete-agreement') && (
+        <DeleteAgreementModal
+          agreement={agreement}
+          open={opensDelete}
+          handleClose={handleCloseDelete}
+          currentCompany={currentCompany}
+        />
+      )}
+    </Grid>
+  );
 }
--- src/Utils/utils.ts
+++ src/Utils/utils.ts
@@ -1,188 +1,188 @@
-import { addYears, differenceInDays, differenceInHours } from "date-fns";
-import i18n from "../i18n";
-import { API_KEY } from "../Constants/constant";
+import { addYears, differenceInDays, differenceInHours } from 'date-fns';
+import i18n from '../i18n';
+import { API_KEY } from '../Constants/constant';
 
 export const extractFileName = (file: any) => {
-    const isSmallScreen = window.innerWidth < 450;
-    return file ? file?.name?.slice(0, isSmallScreen ? 10 : 25) + "..." : "";
+  const isSmallScreen = window.innerWidth < 450;
+  return file ? file?.name?.slice(0, isSmallScreen ? 10 : 25) + '...' : '';
 };
 
 export function formatDate(date: any) {
-    if (!date) date = new Date();
-    const day = String(date.getDate()).padStart(2, "0");
-    const month = String(date.getMonth() + 1).padStart(2, "0");
-    const year = date.getFullYear();
-    return `${day}/${month}/${year}`;
+  if (!date) date = new Date();
+  const day = String(date.getDate()).padStart(2, '0');
+  const month = String(date.getMonth() + 1).padStart(2, '0');
+  const year = date.getFullYear();
+  return `${day}/${month}/${year}`;
 }
 
 export function formatDateAndTime(dateString: any = new Date()) {
-    const currentLanguage = i18n.language;
-    const options: any = {
-        year: "numeric",
-        day: "2-digit",
-        month: "long",
-        weekday: "long",
-        hour: "2-digit",
-        minute: "2-digit",
-        hour12: true,
-    };
+  const currentLanguage = i18n.language;
+  const options: any = {
+    year: 'numeric',
+    day: '2-digit',
+    month: 'long',
+    weekday: 'long',
+    hour: '2-digit',
+    minute: '2-digit',
+    hour12: true,
+  };
 
-    const date = new Date(dateString);
-    const formattedDate = date.toLocaleDateString(currentLanguage, options);
+  const date = new Date(dateString);
+  const formattedDate = date.toLocaleDateString(currentLanguage, options);
 
-    return formattedDate.replaceAll(",", "");
+  return formattedDate.replaceAll(',', '');
 }
 
 export function formatCurrency(value: any) {
-    return new Intl.NumberFormat("tr", {
-        style: "currency",
-        currency: "TRY",
-    }).format(value as number);
+  return new Intl.NumberFormat('tr', {
+    style: 'currency',
+    currency: 'TRY',
+  }).format(value as number);
 }
 
 export function parseCurrency(value: any) {
-    return parseFloat(value.slice(1).replaceAll(".", ""));
+  return parseFloat(value.slice(1).replaceAll('.', ''));
 }
 
 export function parseDateFromString(dateString: String) {
-    const [day, month, year] = dateString.split("/");
-    return new Date(parseInt(year), parseInt(month) - 1, parseInt(day));
+  const [day, month, year] = dateString.split('/');
+  return new Date(parseInt(year), parseInt(month) - 1, parseInt(day));
 }
 
 export function remainingTime(endDate: string) {
-    const currentLanguage = i18n.language;
-    // Parse Dates back to Date format
-    const parsedEndDate = parseDateFromString(endDate);
+  const currentLanguage = i18n.language;
+  // Parse Dates back to Date format
+  const parsedEndDate = parseDateFromString(endDate);
 
-    // Calculate the difference between the dates in days
-    const difference = differenceInDays(parsedEndDate, new Date());
-    const differenceHours = differenceInHours(parsedEndDate, new Date());
+  // Calculate the difference between the dates in days
+  const difference = differenceInDays(parsedEndDate, new Date());
+  const differenceHours = differenceInHours(parsedEndDate, new Date());
 
-    // Calculate remaining Years, months and days
-    const years = Math.floor(difference / 365);
-    const months = Math.floor((difference % 365) / 30);
-    const days = difference % 30;
+  // Calculate remaining Years, months and days
+  const years = Math.floor(difference / 365);
+  const months = Math.floor((difference % 365) / 30);
+  const days = difference % 30;
+  console.log(years, months, days, differenceHours);
 
-    if (years === 0 && months === 0 && days === 0)
-        return currentLanguage === "en-EN"
-            ? `${differenceHours} hours`
-            : `${differenceHours} saat`;
-    // return `${differenceHours} hours remaining`;
+  if (years === 0 && months === 0 && days === 0 && differenceHours > 0)
+    return currentLanguage === 'en-EN'
+      ? `${differenceHours} hours`
+      : `${differenceHours} saat`;
+  // return `${differenceHours} hours remaining`;
 
-    if (years === 0 && months === 0 && days === 0 && differenceHours === 0)
-        return currentLanguage === "en-EN"
-            ? `Agreement is expired`
-            : `Anlaşma süresi doldu`;
-    // return `Agreement is expired`;
+  if (years <= 0 && months <= 0 && days <= 0 && differenceHours <= 0)
+    return currentLanguage === 'en-EN'
+      ? `Agreement is expired`
+      : `Anlaşma süresi doldu`;
+  // return `Agreement is expired`;
 
-    return `${years > 0 ? years + i18n.t(" years ") : ""}${
-        months > 0 ? months + i18n.t(" months ") : ""
-    }${days > 0 ? days + i18n.t(" days ") : ""}`;
+  return `${years > 0 ? years + i18n.t(' years ') : ''}${
+    months > 0 ? months + i18n.t(' months ') : ''
+  }${days > 0 ? days + i18n.t(' days ') : ''}`;
 }
 
 // Start Date : saleCreatedAt --- Guarantee Time : selectedGuaranteeTime (1 Year, 2 Year | 1 Yıl, 2 Yıl)
 export function calcGuaranteeExpireDate(
-    startDate: Date,
-    guaranteeTime: number
+  startDate: Date,
+  guaranteeTime: number
 ) {
-    return addYears(startDate, guaranteeTime);
+  return addYears(startDate, guaranteeTime);
 }
 
 export async function reverseGeocode(lat: any, lng: any) {
-    try {
-        const clickedAddress = await fetch(
-            `https://geocode.maps.co/reverse?lat=${lat}&lon=${lng}&api_key=${API_KEY}`
-        );
-        const data = await clickedAddress.json();
-        return data.address;
-    } catch (err) {
-        console.log(err);
-    }
+  try {
+    const clickedAddress = await fetch(
+      `https://geocode.maps.co/reverse?lat=${lat}&lon=${lng}&api_key=${API_KEY}`
+    );
+    const data = await clickedAddress.json();
+    return data.address;
+  } catch (err) {
+    console.log(err);
+  }
 }
 
 export async function forwardGeocode(address: any) {
-    try {
-        const clickedAddress = await fetch(
-            `https://geocode.maps.co/search?q=${address}&api_key=${API_KEY}`
-        );
-        const data = await clickedAddress.json();
-        return data;
-    } catch (err) {
-        console.log(err);
-    }
+  try {
+    const clickedAddress = await fetch(
+      `https://geocode.maps.co/search?q=${address}&api_key=${API_KEY}`
+    );
+    const data = await clickedAddress.json();
+    return data;
+  } catch (err) {
+    console.log(err);
+  }
 }
 
 export function createTurkishEventNames(event: string) {
-    //: Warehouse
-    if (event === "Add Item") return "Malzeme Ekleme";
-    if (event === "Update Item") return "Malzeme Güncelleme";
-    if (event === "Delete Item") return "Malzeme Silme";
+  //: Warehouse
+  if (event === 'Add Item') return 'Malzeme Ekleme';
+  if (event === 'Update Item') return 'Malzeme Güncelleme';
+  if (event === 'Delete Item') return 'Malzeme Silme';
 
-    //: Employees
-    if (event === "Add Employee") return "Çalışan Ekleme";
-    if (event === "Update Employee") return "Çalışan Güncelleme";
-    if (event === "Delete Employee") return "Çalışan Silme";
+  //: Employees
+  if (event === 'Add Employee') return 'Çalışan Ekleme';
+  if (event === 'Update Employee') return 'Çalışan Güncelleme';
+  if (event === 'Delete Employee') return 'Çalışan Silme';
 
-    //: User
-    if (event === "Update User Name") return "Kullanıcı İsmi Güncelleme";
-    if (event === "Update User Avatar") return "Kullanıcı Avatarı Güncelleme";
-    if (event === "Update User Email") return "Kullanıcı Maili Güncelleme";
-    if (event === "Update User Password") return "Kullanıcı Şifre Güncelleme";
+  //: User
+  if (event === 'Update User Name') return 'Kullanıcı İsmi Güncelleme';
+  if (event === 'Update User Avatar') return 'Kullanıcı Avatarı Güncelleme';
+  if (event === 'Update User Email') return 'Kullanıcı Maili Güncelleme';
+  if (event === 'Update User Password') return 'Kullanıcı Şifre Güncelleme';
 
-    //: Company
-    if (event === "Add Company") return "Şirket Ekleme";
-    if (event === "Update Company") return "Şirket Güncelleme";
-    if (event === "Delete Company") return "Şirket Silme";
+  //: Company
+  if (event === 'Add Company') return 'Şirket Ekleme';
+  if (event === 'Update Company') return 'Şirket Güncelleme';
+  if (event === 'Delete Company') return 'Şirket Silme';
 
-    //: Sales
-    if (event === "Add Sale") return "Satış Ekleme";
-    if (event === "Delete Sale") return "Satış Silme";
+  //: Sales
+  if (event === 'Add Sale') return 'Satış Ekleme';
+  if (event === 'Delete Sale') return 'Satış Silme';
 
-    //: Agreements
-    if (event === "Add Agreement") return "Anlaşma Ekleme";
-    if (event === "Delete Agreement") return "Anlaşma Silme";
-    if (event === "Update Agreement") return "Anlaşma Güncelleme";
+  //: Agreements
+  if (event === 'Add Agreement') return 'Anlaşma Ekleme';
+  if (event === 'Delete Agreement') return 'Anlaşma Silme';
+  if (event === 'Update Agreement') return 'Anlaşma Güncelleme';
 
-    //: Negotiates
-    if (event === "Add Negotiate") return "Görüşme Ekleme";
+  //: Negotiates
+  if (event === 'Add Negotiate') return 'Görüşme Ekleme';
 }
 
 export function generateExportButtonClassName(pathname: string) {
-    if (pathname === "/employees") return "export-btn-employee";
-    if (pathname === "/warehouse") return "export-btn-warehouse";
-    if (pathname === "/companies") return "export-btn-companies";
-    if (pathname === "/accounting") return "export-btn-accounting";
-    if (pathname.includes("/companies/")) return "export-btn-sales";
+  if (pathname === '/employees') return 'export-btn-employee';
+  if (pathname === '/warehouse') return 'export-btn-warehouse';
+  if (pathname === '/companies') return 'export-btn-companies';
+  if (pathname === '/accounting') return 'export-btn-accounting';
+  if (pathname.includes('/companies/')) return 'export-btn-sales';
 }
 
 export function generateSearchInputClassName(pathname: string) {
-    if (pathname === "/employees") return "search-input-employee";
-    if (pathname === "/warehouse") return "search-input-warehouse";
-    if (pathname === "/companies") return "search-input-companies";
-    if (pathname === "/accounting") return "search-input-accounting";
-    if (pathname.includes("/companies/")) return "search-input-sales";
-    if (pathname.includes("/notifications"))
-        return "search-input-notifications";
+  if (pathname === '/employees') return 'search-input-employee';
+  if (pathname === '/warehouse') return 'search-input-warehouse';
+  if (pathname === '/companies') return 'search-input-companies';
+  if (pathname === '/accounting') return 'search-input-accounting';
+  if (pathname.includes('/companies/')) return 'search-input-sales';
+  if (pathname.includes('/notifications')) return 'search-input-notifications';
 }
 
 // Test
 export function formatTimestampToDate({
-    seconds,
-    nanoseconds,
+  seconds,
+  nanoseconds,
 }: {
-    seconds: any;
-    nanoseconds: any;
+  seconds: any;
+  nanoseconds: any;
 }) {
-    const date = new Date(seconds * 1000 + nanoseconds / 1000000); // Timestamp'i milisaniyeye çevir
-    const day = String(date.getDate()).padStart(2, "0");
-    const month = String(date.getMonth() + 1).padStart(2, "0"); // Aylar 0-11 arasında olduğundan 1 ekliyoruz
-    const year = date.getFullYear();
+  const date = new Date(seconds * 1000 + nanoseconds / 1000000); // Timestamp'i milisaniyeye çevir
+  const day = String(date.getDate()).padStart(2, '0');
+  const month = String(date.getMonth() + 1).padStart(2, '0'); // Aylar 0-11 arasında olduğundan 1 ekliyoruz
+  const year = date.getFullYear();
 
-    return `${day}/${month}/${year}`;
+  return `${day}/${month}/${year}`;
 }
 
 export function timestampToDate({ seconds }: { seconds: any }) {
-    // Timestamp'i milisaniyeye çevir (saniyeyi 1000 ile çarp)
-    const date = new Date(seconds * 1000);
-    return date;
+  // Timestamp'i milisaniyeye çevir (saniyeyi 1000 ile çarp)
+  const date = new Date(seconds * 1000);
+  return date;
 }
